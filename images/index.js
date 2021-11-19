e16f5c6aa0d870f0466da0442b03cd26c3e7937d
michaellarrubis <michaellarrubis@gmail.com>
2021-11-19 11:04:36 +0800

    Build: images

---
+++ images/index.js
@@ -0,0 +1,18 @@
+const S3BucketJobseeker = process.env.S3_BUCKET_URL
+
+export const BossjobLogo = `${S3BucketJobseeker}/Bossjob.svg`
+export const BriefcaseIcon = `${S3BucketJobseeker}/BriefcaseIcon.svg`
+export const CommissionIcon = `${S3BucketJobseeker}/CommissionIcon.svg`
+export const DropdownArrowIcon = `${S3BucketJobseeker}/DropdownArrowIcon.svg`
+export const EducationIcon = `${S3BucketJobseeker}/EducationIcon.svg`
+export const EmployeeStockIcon = `${S3BucketJobseeker}/EmployeeStockIcon.svg`
+export const EquityIcon = `${S3BucketJobseeker}/EquityIcon.svg`
+export const HousingAllowanceIcon = `${S3BucketJobseeker}/HousingAllowanceIcon.svg`
+export const LocationIcon = `${S3BucketJobseeker}/LocationIcon.svg`
+export const MealAllowanceIcon = `${S3BucketJobseeker}/MealAllowanceIcon.svg`
+export const NotificationIcon = `${S3BucketJobseeker}/NotificationIcon.svg`
+export const OtherAllowanceIcon = `${S3BucketJobseeker}/OtherAllowanceIcon.svg`
+export const PerformanceBonusIcon = `${S3BucketJobseeker}/PerformanceBonusIcon.svg`
+export const SalaryIcon = `${S3BucketJobseeker}/SalaryIcon.svg`
+export const TelcomAllowanceIcon = `${S3BucketJobseeker}/TelcomAllowanceIcon.svg`
+export const TransportAllowanceIcon = `${S3BucketJobseeker}/TransportAllowanceIcon.svg`
