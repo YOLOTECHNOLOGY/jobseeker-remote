b0cd5b8f19823689fff534935cb20fe21e356167
michaellarrubis <michaellarrubis@gmail.com>
2022-03-29 09:31:56 +0800

    Update: Use jobseeker api for companies

--- store/reducers/companies/index.js
+++ store/reducers/companies/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux'
 
-import fetchFeaturedCompaniesReducer from './fetchFeaturedCompanies'
 import fetchCompanyDetailReducer from './fetchCompanyDetail'
 import fetchFeaturedCompaniesListReducer from './fetchFeaturedCompaniesList'
 import fetchSimilarCompanyReducer from './fetchSimilarCompany'
@@ -8,7 +7,6 @@ import fetchCompanySuggestionsReducer from './fetchCompanySuggestions'
 import fetchCompanyFilterReducer from './fetchCompanyFilter'
 
 const companiesReducers = combineReducers({
-  featuredCompanies: fetchFeaturedCompaniesReducer,
   companyDetail:fetchCompanyDetailReducer,
   fetchFeaturedCompaniesList: fetchFeaturedCompaniesListReducer,
   fetchSimilarCompany: fetchSimilarCompanyReducer,
