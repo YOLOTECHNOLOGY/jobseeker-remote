99e09281fe887e85043f454669ed0eab12b534a0
claudiachia <claudia@yolotechnology.com>
2022-05-10 18:57:52 +0800

    Test: google ad implementation

--- helpers/useAdSlot.js
+++ helpers/useAdSlot.js
@@ -2,30 +2,36 @@ import { useEffect } from 'react'
 
 function useAdSlot({ mapping, sizes, id, adUnit, isTransitioning }) {
   useEffect(() => {
-    if (!isTransitioning && typeof window !== undefined) {
-      const { googletag } = window
-      googletag.cmd.push(function () {
-        const adMapping = googletag.sizeMapping()
-        Object.keys(mapping).forEach((breakpoint) => {
-          adMapping.addSize([Number(breakpoint), 0], [mapping[breakpoint]])
+    try {
+      // @ts-ignore
+      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+      
+      if (!isTransitioning && typeof window !== undefined) {
+        const { googletag } = window
+        googletag.cmd.push(function () {
+          const adMapping = googletag.sizeMapping()
+          Object.keys(mapping).forEach((breakpoint) => {
+            adMapping.addSize([Number(breakpoint), 0], [mapping[breakpoint]])
+          })
+          const builtMapping = adMapping.build()
+
+          googletag
+            .defineSlot(`/21858999436/${adUnit}`, sizes, `div-gpt-ad-${id}`)
+            .defineSizeMapping(builtMapping)
+            .addService(googletag.pubads())
+          googletag.enableServices()
         })
-        const builtMapping = adMapping.build()
 
-        googletag
-          .defineSlot(`/21858999436/${adUnit}`, sizes, `div-gpt-ad-${id}`)
-          .defineSizeMapping(builtMapping)
-          .addService(googletag.pubads())
-        googletag.enableServices()
-      })
+        googletag.pubads().collapseEmptyDivs()
 
-      googletag.pubads().collapseEmptyDivs()
-
-      googletag.cmd.push(function () {
-        googletag.display(`div-gpt-ad-${id}`)
-      })
+        googletag.cmd.push(function () {
+          googletag.display(`div-gpt-ad-${id}`)
+        })
+      }
+    } catch (err) {
+      console.error(err)
     }
   }, [mapping, sizes, adUnit, id, isTransitioning])
 }
-
 
 export default useAdSlot
