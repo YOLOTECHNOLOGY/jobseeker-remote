45e7f4ec2dadba0d080ca556fe6b7d3e6e60b419
James Bong <jamesbong5179@gmail.com>
2022-05-11 20:13:49 +0800

    Fix: sort option in job search page

--- pages/jobs-hiring/[keyword].tsx
+++ pages/jobs-hiring/[keyword].tsx
@@ -318,6 +318,8 @@ const JobSearchPage = (props: JobSearchPageProps) => {
     const { keyword, ...rest } = router.query
     const sortOption = val.length > 0 ? 2 : 1
     
+    setSort(sortOption)
+    
     // Check if the search value matched with the reserved location keyword
     const matchedLocation = locList.filter((loc) => {
       return loc.value.toLowerCase() === searchValue.toLowerCase()
