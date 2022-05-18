56f6aa396f37497598cb74c7bf00ab7ddd733349
claudiachia <claudia@yolotechnology.com>
2022-05-18 17:24:03 +0800

    Test: useAdSlot changes

--- helpers/useAdSlot.js
+++ helpers/useAdSlot.js
@@ -7,15 +7,11 @@ function useAdSlot({ mapping, sizes, id, adUnit, isTransitioning }) {
         const { googletag } = window
         if (googletag.apiReady) {
           googletag.cmd.push(function () {
-            // debugger
             const adMapping = googletag.sizeMapping()
             Object.keys(mapping).forEach((breakpoint) => {
-              // debugger
-              adMapping.addSize([Number(breakpoint), 0], [mapping[breakpoint]])
+              adMapping.addSize([Number(breakpoint), 0], mapping[breakpoint])
             })
             const builtMapping = adMapping.build()
-
-            console.log('useAdSlot 2', adUnit, id, sizes)
 
             googletag
             .defineSlot(`/21858999436/${adUnit}`, sizes, `div-gpt-ad-${id}`)
@@ -24,7 +20,6 @@ function useAdSlot({ mapping, sizes, id, adUnit, isTransitioning }) {
             
             googletag.enableServices()
           })
-          // console.log('useAdSlot ad id', id)
           googletag.pubads().collapseEmptyDivs()
 
           googletag.cmd.push(function () {
