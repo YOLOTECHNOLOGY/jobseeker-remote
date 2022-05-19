1f08d6f0db2d029ab1b11cb11809ce2f6a419ed4
claudiachia <claudia@yolotechnology.com>
2022-05-19 12:19:14 +0800

    Test: ad size mapping

--- helpers/useAdSlot.js
+++ helpers/useAdSlot.js
@@ -7,12 +7,21 @@ function useAdSlot({ mapping, sizes, id, adUnit, isTransitioning }) {
         const { googletag } = window
         if (googletag.apiReady) {
           googletag.cmd.push(function () {
+            // debugger
             const adMapping = googletag.sizeMapping()
             Object.keys(mapping).forEach((breakpoint) => {
-              adMapping.addSize([Number(breakpoint), 0], mapping[breakpoint])
+              console.log(
+                '[Number(breakpoint), 0], [mapping[breakpoint]]',
+                [Number(breakpoint), 0],
+                [mapping[breakpoint]]
+              )
+              adMapping.addSize([Number(breakpoint), 0], [mapping[breakpoint]])
             })
             const builtMapping = adMapping.build()
 
+            console.log('adMapping', adMapping)
+            console.log('builtMapping', builtMapping)
+            
             googletag
             .defineSlot(`/21858999436/${adUnit}`, sizes, `div-gpt-ad-${id}`)
             .defineSizeMapping(builtMapping)
