dcce26dc55ebf74a36e5943974740687f109939c
winston-lim <winston_lim1@hotmail.com>
2022-05-30 14:55:48 +0800

    Update Layout to render verify email modal

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -11,6 +11,8 @@ import classNamesCombined from 'classnames'
 import { getCookie } from '../../helpers/cookies'
 import { Link } from '@mui/material'
 import MaterialAlert from '../MaterialAlert/MaterialAlert'
+import ModalVerifyEmail from '../ModalVerifyEmail'
+import Text from '../Text'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -20,26 +22,37 @@ interface LayoutProps {
 const Layout = ({ children, className }: LayoutProps) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [isEmailVerified, setIsEmailVerified] = useState(false)
+  const [isShowModal, setIsShowModal] = useState(false)
   useEffect(() => {
     setIsAuthenticated(getCookie('accessToken') ? true : false)
     const userCookie = getCookie('user')
-    setIsEmailVerified(userCookie?.is_email_verified)
+    setIsEmailVerified(userCookie?.is_email_verify)
   }, [])
-  
+
   return (
     <div className={classNamesCombined([styles.container, className])}>
       {isAuthenticated && !isEmailVerified && (
-        <MaterialAlert
-          open={true}
-          severity='info'
-        >
-          Please verify your email address. <Link>Verify now.</Link>
+        <MaterialAlert open={true} severity='info'>
+          <Text>
+            Please verify your email address.{' '}
+            <Link onClick={() => setIsShowModal(true)} sx={{ cursor: 'pointer' }}>
+              Verify now.
+            </Link>
+          </Text>
         </MaterialAlert>
       )}
       <Header />
       <HamburgerMenu />
       {children}
       <Footer />
+      <ModalVerifyEmail
+        email={isAuthenticated && getCookie('user') ? getCookie('user').email : ''}
+        isShowModal={isShowModal}
+        handleModal={(isShow: boolean) => {
+          setIsShowModal(isShow)
+          setIsEmailVerified(getCookie('user').is_email_verify)
+        }}
+      />
     </div>
   )
 }
