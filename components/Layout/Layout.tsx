451972dd624b1448f6befc7ef0e1d0be134a67a2
winston-lim <winston_lim1@hotmail.com>
2022-05-30 17:25:09 +0800

    Update Layout to setCookie for managing when to auto show verify email modal

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ import HamburgerMenu from 'components/HamburgerMenu'
 /* Styles */
 import styles from './Layout.module.scss'
 import classNamesCombined from 'classnames'
-import { getCookie } from '../../helpers/cookies'
+import { setCookie, getCookie } from '../../helpers/cookies'
 import { Link } from '@mui/material'
 import MaterialAlert from '../MaterialAlert/MaterialAlert'
 import ModalVerifyEmail from '../ModalVerifyEmail'
@@ -23,11 +23,22 @@ const Layout = ({ children, className }: LayoutProps) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [isEmailVerified, setIsEmailVerified] = useState(false)
   const [isShowModal, setIsShowModal] = useState(false)
+
   useEffect(() => {
+    const authCookie = getCookie('accessToken')
     setIsAuthenticated(getCookie('accessToken') ? true : false)
     const userCookie = getCookie('user')
     setIsEmailVerified(userCookie?.is_email_verify)
+    const isVerifyEmailModalClosed = getCookie('isVerifyEmailModalClosed')
+    setIsShowModal(!isVerifyEmailModalClosed && !!authCookie && !!!userCookie.is_email_verify)
   }, [])
+
+  if (isShowModal) {
+    // if modal is show, body should be fixed to disable scrolling
+    document.body.style.position = 'fixed'
+    document.body.style.width = '100%'
+    document.body.style.top = `-${window.scrollY}px`
+  }
 
   return (
     <div className={classNamesCombined([styles.container, className])}>
@@ -51,6 +62,13 @@ const Layout = ({ children, className }: LayoutProps) => {
         handleModal={(isShow: boolean) => {
           setIsShowModal(isShow)
           setIsEmailVerified(getCookie('user').is_email_verify)
+          setCookie('isVerifyEmailModalClosed', true)
+          // once modal is closed, body should not be fixed to enable scrolling
+          const scrollY = document.body.style.top
+          document.body.style.position = ''
+          document.body.style.top = ''
+          // retrieve previous scroll position
+          window.scrollTo(0, parseInt(scrollY || '0') * -1)
         }}
       />
     </div>
