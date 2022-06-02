1887ec6497f4b3ce991d52d0869ed563fdd8342f
winston-lim <winston_lim1@hotmail.com>
2022-06-02 13:51:59 +0800

    Refactor code

--- helpers/handleInput.js
+++ helpers/handleInput.js
@@ -1,9 +1,11 @@
 function numberWithDecimalFilter(val) {
   return /^\d*\.?\d*$/.test(val) // Allow digits and '.' only, using a RegExp
 }
+
 function numberFilter(val) {
   return /^[0-9]*$/.test(val)
 }
+
 export const handleNumericInput = (val) => {
   return numberFilter(val) ? val : val.slice(0, -1)
 }
