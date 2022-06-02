f3d4ec6f642d8a6d3c0ebbc07ce94c362d97cd1e
winston-lim <winston_lim1@hotmail.com>
2022-06-02 15:06:40 +0800

    Update AdSlot to not use useSlot custom hook

--- components/AdSlot/AdSlot.tsx
+++ components/AdSlot/AdSlot.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import useAdSlot from 'helpers/useAdSlot'
+
+import React, { useEffect } from 'react'
+// import useAdSlot from 'helpers/useAdSlot'
 import useTransitionState from 'helpers/useTransitionState'
 import ads from 'lib/config/adConfig'
 
@@ -14,18 +15,58 @@ const AdSlot = ({ adSlot }: adSlotProps) => {
 
   if (process.env.ENV === 'development') {
   // if (process.env.ENV === 'production') {
-    
-    useAdSlot({
-      mapping: ad.mapping ? ad.mapping : {},
-      id: ad.id,
-      adUnit: ad.adUnit,
-      sizes: ad.sizes,
-      isTransitioning,
-    })
+    // useAdSlot({
+    //   mapping: ad.mapping ? ad.mapping : {},
+    //   id: ad.id,
+    //   adUnit: ad.adUnit,
+    //   sizes: ad.sizes,
+    //   isTransitioning,
+    // })
+    useEffect(() => {
+      try {
+        if (!isTransitioning && typeof window !== undefined) {
+          const { googletag } = window
+          if (googletag.apiReady) {
+            googletag.cmd.push(function () {
+              // debugger
+              const adMapping = googletag.sizeMapping()
+              Object.keys(ad.mapping).forEach((breakpoint) => {
+                // debugger
+                const isSingleSizePerMapping = ad.mapping[breakpoint].length < 1
+                console.log(
+                  'addSize',
+                  [Number(breakpoint), 0],
+                  isSingleSizePerMapping ? ad.mapping[breakpoint] : [ad.mapping[breakpoint]]
+                )
+                adMapping.addSize([Number(breakpoint), 0], isSingleSizePerMapping ? ad.mapping[breakpoint] : [ad.mapping[breakpoint]])
+              })
+              const builtMapping = adMapping.build()
+
+              console.log('adMapping', adMapping)
+              console.log('builtMapping', builtMapping)
+
+              googletag
+              .defineSlot(`/21858999436/${ad.adUnit}`, ad.sizes, `div-gpt-ad-${ad.id}`)
+              .defineSizeMapping(builtMapping)
+              .addService(googletag.pubads())
+
+              googletag.enableServices()
+            })
+            googletag.pubads().collapseEmptyDivs()
+
+            googletag.cmd.push(function () {
+              googletag.display(`div-gpt-ad-${ad.id}`)
+            })
+          }
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    }, [ad.mapping, ad.sizes, ad.adUnit, ad.id, isTransitioning])
   }
 
   return <div id={`div-gpt-ad-${ad.id}`} />
 }
 
 export default AdSlot
-// export default memo(AdSlot)
+// export default memo(AdSlot) 
