9bfdec5864648f2c852f77a35d4cdf4aece6cad7
Allen Wong <allen@yolotechnology.com>
2022-06-21 17:20:47 +0800

    Build: redirect update

--- store/sagas/auth/login.js
+++ store/sagas/auth/login.js
@@ -65,10 +65,21 @@ function* loginReq(actions) {
         (loginData.is_profile_update_required || !loginData.is_profile_completed)
           ? '/jobseeker-complete-profile/1'
           : `/jobs-hiring/job-search`
-
+      
       if (redirect) {
         if (redirect.includes(process.env.OLD_PROJECT_URL)) {
-          url = `${redirect}&token=${loginData.authentication.access_token}`
+          let redirectUrl = redirect
+          const delimiter = '/jobseeker-login-redirect?redirectUrl='
+          const redirectArray = redirect.split(delimiter)
+          if (redirectArray.length === 2) {
+            const queryParam = redirectArray[1]
+            const encodedQueryParam = encodeURIComponent(queryParam)
+            let encodedRedirect = ''
+            encodedRedirect.concat(redirectArray[0], delimiter, encodedQueryParam)
+            redirectUrl = redirectArray[0] + delimiter + encodedQueryParam
+          }
+
+          url = `${redirectUrl}&token=${loginData.authentication.access_token}`
         } else {
           url = redirect
         }
