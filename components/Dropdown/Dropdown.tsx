04723da2d1668a57204954a7fa42da12ef07c0cb
winston-lim <winston_lim1@hotmail.com>
2022-07-12 16:51:17 +0800

    Feature: [2240] build Dropdown component

---
+++ components/Dropdown/Dropdown.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+import { Menu } from '@mui/material'
+import { MoreIcon } from '../../images'
+
+type DropdownProps = {
+  children?: any
+}
+
+const Dropdown = ({ children }: DropdownProps) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <div>
+      <div onClick={handleClick}>
+        <img src={MoreIcon} width='5' height='20' />
+      </div>
+      <Menu anchorEl={anchorEl} open={open} onClick={handleClose} onClose={handleClose}>
+        {children}
+      </Menu>
+    </div>
+  )
+}
+
+export default Dropdown
