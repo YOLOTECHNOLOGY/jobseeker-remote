42deab60fac3b2e1b6af18175ebe1ed473c668b1
James Bong <jamesbong5179@gmail.com>
2022-07-13 00:35:39 +0800

    Fix: canonical link issue on company page

--- components/Company/CompanyProfileLayout/CompanyProfileLayout.tsx
+++ components/Company/CompanyProfileLayout/CompanyProfileLayout.tsx
@@ -80,15 +80,14 @@ const CompanyProfileLayout = ({
     if (similarCompaniesResponse) setSimilarCompanies(similarCompaniesResponse)
   }, [similarCompaniesResponse])
 
-  const initialCanonicalText = `/company/${company.name.split(' ').join('-')}`
   const additionalCanonicalText =
     currentTab == 'jobs' ? '/jobs' : currentTab == 'life' ? '/life' : ''
-  const finalCanonicalText = initialCanonicalText + additionalCanonicalText
-  const companyUrl = company?.company_url || '/'
-
+  const companyUrl = company.company_url
+  const canonicalUrl = companyUrl + additionalCanonicalText
+  
   return (
     <Layout>
-      <SEO title={seoMetaTitle} description={seoMetaDescription} canonical={finalCanonicalText} imageUrl={company.logo_url}/>
+      <SEO title={seoMetaTitle} description={seoMetaDescription} canonical={canonicalUrl} imageUrl={company.logo_url}/>
       <div className={styles.company}>
         <div className={styles.companyContent}>
           <div className={styles.companyHeader}>
