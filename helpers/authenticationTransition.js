c0a2cf700f29f47ba2f6317651851624d6704c82
James Bong <jamesbong5179@gmail.com>
2022-07-14 16:06:38 +0800

    Update: [1932] Redirect link in jobseeker signup

--- helpers/authenticationTransition.js
+++ helpers/authenticationTransition.js
@@ -4,18 +4,23 @@ import { getCookie } from 'helpers/cookies'
 export const authPathToOldProject = (accessToken, redirectUrl) => {
   const authToken = accessToken ? accessToken : getCookie('accessToken')
   const isAppRedirectModalClosed = getCookie('isAppRedirectModalClosed') ? 'true': 'false'
-  const oldProjectUrl = `${process.env.OLD_PROJECT_URL}/jobseeker-login-redirect`
-  // const oldProjectPath = {
-  //   pathname: oldProjectUrl,
-  //   query: {
-  //     token: authToken,
-  //     redirectUrl: redirectUrl,
-  //   },
-  // }
-  // return oldProjectPath
-  if (authToken) {
-    return `${oldProjectUrl}?redirectUrl=${redirectUrl}&token=${authToken}&isAppRedirectModalClosed=${isAppRedirectModalClosed}`
+  
+  redirectUrl = `${process.env.OLD_PROJECT_URL}/jobseeker-login-redirect?redirectUrl=${redirectUrl}`
+  const delimiter = '/jobseeker-login-redirect?redirectUrl='
+  const redirectArray = redirectUrl.split(delimiter)
+
+  // Encode url params 
+  if (redirectArray.length === 2) {
+    const queryParam = redirectArray[1]
+    const encodedQueryParam = encodeURIComponent(queryParam)
+    let encodedRedirect = ''
+    encodedRedirect.concat(redirectArray[0], delimiter, encodedQueryParam)
+    redirectUrl = redirectArray[0] + delimiter + encodedQueryParam
   }
 
-  return `${oldProjectUrl}?redirectUrl=${redirectUrl}&isAppRedirectModalClosed=${isAppRedirectModalClosed}`
+  if (authToken) {
+    return `${redirectUrl}&token=${authToken}&isAppRedirectModalClosed=${isAppRedirectModalClosed}`
+  }
+
+  return `${redirectUrl}&isAppRedirectModalClosed=${isAppRedirectModalClosed}`
 }
--- store/sagas/auth/login.js
+++ store/sagas/auth/login.js
@@ -4,6 +4,8 @@ import { setCookie } from 'helpers/cookies'
 
 import { LOGIN_REQUEST } from 'store/types/auth/login'
 
+import { authPathToOldProject } from 'helpers/authenticationTransition'
+
 import {
   loginSuccess,
   loginFailed,
@@ -68,18 +70,9 @@ function* loginReq(actions) {
 
       if (redirect) {
         if (redirect.includes(process.env.OLD_PROJECT_URL)) {
-          let redirectUrl = redirect
-          const delimiter = '/jobseeker-login-redirect?redirectUrl='
-          const redirectArray = redirect.split(delimiter)
-          if (redirectArray.length === 2) {
-            const queryParam = redirectArray[1]
-            const encodedQueryParam = encodeURIComponent(queryParam)
-            let encodedRedirect = ''
-            encodedRedirect.concat(redirectArray[0], delimiter, encodedQueryParam)
-            redirectUrl = redirectArray[0] + delimiter + encodedQueryParam
-          }
-
-          url = `${redirectUrl}&token=${loginData.authentication.access_token}`
+          const newUrl = new URL(redirect)
+          
+          url = authPathToOldProject(loginData.authentication.access_token, newUrl.pathname + newUrl.search)
         } else {
           url = redirect
         }
