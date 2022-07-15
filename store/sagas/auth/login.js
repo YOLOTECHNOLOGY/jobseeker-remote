4f838165f57da559b4ff27515682acb65a00dd4a
James Bong <jamesbong5179@gmail.com>
2022-07-15 14:29:00 +0800

    Update: check redirect link for login and sign up redirect

--- store/sagas/auth/login.js
+++ store/sagas/auth/login.js
@@ -69,7 +69,7 @@ function* loginReq(actions) {
           : `/jobs-hiring/job-search`
 
       if (redirect) {
-        if (redirect.includes(process.env.OLD_PROJECT_URL)) {
+        if (redirect.includes(process.env.OLD_PROJECT_URL) && !redirect.includes('/jobseeker-login-redirect')) {
           const newUrl = new URL(redirect)
           
           url = authPathToOldProject(loginData.authentication.access_token, newUrl.pathname + newUrl.search)
