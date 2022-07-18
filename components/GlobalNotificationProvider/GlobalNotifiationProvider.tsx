7e5a28465b2eba6e9d1905c54c258279e20b1c61
winston-lim <winston_lim1@hotmail.com>
2022-07-18 15:17:31 +0800

    Refactor: [1920] remove unused imports and add NotificationBar back to prevent merge conflicts

--- components/GlobalNotificationProvider/GlobalNotifiationProvider.tsx
+++ components/GlobalNotificationProvider/GlobalNotifiationProvider.tsx
@@ -1,5 +1,4 @@
 import dynamic from 'next/dynamic'
-import React from 'react'
 import { useSelector } from 'react-redux'
 const GlobalNotificationBar = dynamic(() => import('../GlobalNotificationBar'))
 
