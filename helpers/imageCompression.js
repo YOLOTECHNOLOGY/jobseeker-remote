51227ef8d2d3c2b07f35759c44baaed7224e6269
winston-lim <winston_lim1@hotmail.com>
2022-07-21 15:57:31 +0800

    Update: [2316] update imageCompression to support both landscape and portrait images

--- helpers/imageCompression.js
+++ helpers/imageCompression.js
@@ -1,12 +1,18 @@
 import { compressAccurately } from 'image-conversion'
 
-// Specify compression size and width only, in order to maintain aspect ratio
 // Other possible configurations: accuracy, type, scale, height
-export async function compressImage(file, size) {
+export async function compressImage(file, size = 100, aspectRatio = 1, dimension = 400) {
+  let width
+  let height
+  if (aspectRatio > 1) { // landscape
+    width = dimension
+  } else { // portrait
+    height = dimension
+  }
   const result = await compressAccurately(file, {
-    size: size,
-    type: 'image/jpeg',
-    width: 1024,
+    size,
+    width,
+    height
   })
   return result
 }
