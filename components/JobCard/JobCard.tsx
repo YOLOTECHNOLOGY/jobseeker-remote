fcedb1f4552b983c739df4788c625ae7a204bdf6
BossJob-Lee <cctip_lee@proton.me>
2022-08-18 17:25:10 +0800

    Fix: my job application jobcard add tag and application time

--- components/JobCard/JobCard.tsx
+++ components/JobCard/JobCard.tsx
@@ -12,20 +12,25 @@ import styles from './JobCard.module.scss'
 
 import { CompanyIcon, SalaryIcon, LocationIcon, ExpireIcon } from 'images'
 
+const Warning: Array<String> = ['Not suitable']
+const Happy: Array<String> = ['New', 'Under review', 'Shortlisted', 'Interviewed', 'Hired']
+const Quiet: Array<String> = ['Closed', 'No show', 'Withdrawn']
+
 type JobCardProps = {
-  id: number,
-  image: string,
-  title: string,
-  jobType?: string,
-  isFeatured?: boolean,
-  isUrgent?: boolean,
-  company: string,
-  location: string,
-  salary: string,
-  postedAt: string,
-  status: string,
-  selectedId?: number,
+  id: number
+  image: string
+  title: string
+  jobType?: string
+  isFeatured?: boolean
+  isUrgent?: boolean
+  company: string
+  location: string
+  salary: string
+  postedAt: string
+  status: string
+  selectedId?: number
   handleSelectedId?: Function
+  ApplicationStatus?: string
 }
 
 const JobCard = ({
@@ -41,53 +46,69 @@ const JobCard = ({
   postedAt,
   status,
   handleSelectedId,
-  selectedId
+  selectedId,
+  ApplicationStatus
 }: JobCardProps) => {
   const cx = classNames.bind(styles)
   const isSelectedClass = cx({ isSelected: selectedId == id })
 
+  const handleMyJobTagStyle = (type: string) => {
+    if (Warning.includes(type)) {
+      return 'warning'
+    } else if (Happy.includes(type)) {
+      return 'happy'
+    } else if (Quiet.includes(type)) {
+      return 'quiet'
+    }
+  }
+
   return (
-    <div 
-      className={classNamesCombined([styles.JobCard, isSelectedClass])} 
+    <div
+      className={classNamesCombined([styles.JobCard, isSelectedClass])}
       onClick={() => handleSelectedId(id, title)}
     >
       <div className={styles.JobCardImageWrapper}>
-        <div className={styles.JobCardImageContent} style={{ backgroundImage: `url(${image})`}} />
+        <div className={styles.JobCardImageContent} style={{ backgroundImage: `url(${image})` }} />
       </div>
       <div className={styles.JobCardDetailWrapper}>
-        <Text tagName='h1' textStyle='lg' bold className={styles.JobCardDetailTitle}>{truncateWords(title, 60)}</Text>
+        <Text tagName='h1' textStyle='lg' bold className={styles.JobCardDetailTitle}>
+          {truncateWords(title, 60)}
+        </Text>
         <div className={styles.JobCardDetailTag}>
-          {isFeatured && (
-            <JobTag tag='Featured' tagType='featured' />
-          )}
-          {isUrgent && (
-            <JobTag tag='Urgent' tagType='urgent' />
-          )}
-          {jobType && (
-            <JobTag tag={jobType} />
+          {isFeatured && <JobTag tag='Featured' tagType='featured' />}
+          {isUrgent && <JobTag tag='Urgent' tagType='urgent' />}
+          {jobType && <JobTag tag={jobType} />}
+          {ApplicationStatus && (
+            <JobTag tag={ApplicationStatus} tagType={handleMyJobTagStyle(ApplicationStatus)} />
           )}
         </div>
         <div className={styles.JobCardDetailList}>
           <div className={styles.JobCardDetailItem}>
             <img src={CompanyIcon} width='20' height='20' />
-            <Text className={styles.JobCardDetailItemText} textStyle='lg'>{company}</Text>
+            <Text className={styles.JobCardDetailItemText} textStyle='lg'>
+              {company}
+            </Text>
           </div>
           <div className={styles.JobCardDetailItem}>
             <img src={LocationIcon} width='20' height='20' />
-            <Text className={styles.JobCardDetailItemText} textStyle='lg'>{location}</Text>
+            <Text className={styles.JobCardDetailItemText} textStyle='lg'>
+              {location}
+            </Text>
           </div>
           <div className={styles.JobCardDetailItem}>
             <img src={SalaryIcon} width='20' height='20' />
-            <Text className={styles.JobCardDetailItemText} textStyle='lg'>{salary}</Text>
+            <Text className={styles.JobCardDetailItemText} textStyle='lg'>
+              {salary}
+            </Text>
           </div>
         </div>
         {status !== 'active' && (
           <Text textStyle='base' className={styles.JobCardDetailStatus}>
-            <img src={ExpireIcon} height="16" width="16"/>
+            <img src={ExpireIcon} height='16' width='16' />
             <span>This job is no longer hiring</span>
           </Text>
         )}
-        <Text textStyle='sm'>Posted on {postedAt}</Text>
+        <Text textStyle='sm'>Applied on {postedAt}</Text>
       </div>
     </div>
   )
--- components/JobDetail/JobDetail.tsx
+++ components/JobDetail/JobDetail.tsx
@@ -11,7 +11,7 @@ import {
   TimelineSeparator,
   TimelineConnector,
   TimelineContent,
-  TimelineDot,
+  TimelineDot
 } from '@mui/lab'
 import dynamic from 'next/dynamic'
 
@@ -51,7 +51,7 @@ import {
   ExpireIcon,
   LocationPinIcon,
   RateIcon,
-  DefaultAvatar,
+  DefaultAvatar
 } from 'images'
 
 /* Helpers */
@@ -87,11 +87,13 @@ const JobDetail = ({
   handlePostSaveJob,
   handleDeleteSavedJob,
   applicationHistory,
-  config,
+  config
 }: IJobDetailProps) => {
   const router = useRouter()
-  const detailHeaderRef = useRef(null);
-  const [detailHeaderHeight, setDetailHeaderHeight] = useState(detailHeaderRef?.current?.clientHeight || 0);
+  const detailHeaderRef = useRef(null)
+  const [detailHeaderHeight, setDetailHeaderHeight] = useState(
+    detailHeaderRef?.current?.clientHeight || 0
+  )
 
   const userCookie = getCookie('user') || null
   const authCookie = getCookie('accessToken') || null
@@ -110,8 +112,8 @@ const JobDetail = ({
   }, [selectedJob])
 
   useEffect(() => {
-    setDetailHeaderHeight(detailHeaderRef?.current?.clientHeight);
-  }, [detailHeaderHeight]);
+    setDetailHeaderHeight(detailHeaderRef?.current?.clientHeight)
+  }, [detailHeaderHeight])
 
   const handleBenefitIcon = (benefit) => {
     const Icon = `${benefit.replace(/ /g, '')}Icon`
@@ -140,12 +142,14 @@ const JobDetail = ({
 
   const handleVerifyEmailClick = async () => {
     // revalidate verify email status
-    const response = await fetchUserOwnDetailService({accessToken: authCookie})
+    const response = await fetchUserOwnDetailService({ accessToken: authCookie })
     const userDetails = response?.data?.data
     const isVerifiedEmail = userDetails?.is_email_verify
-    if (!isVerifiedEmail) { // email is not verified
-      setIsShowModal(true);
-    } else { // email is verified and user cookie is outdated
+    if (!isVerifiedEmail) {
+      // email is not verified
+      setIsShowModal(true)
+    } else {
+      // email is verified and user cookie is outdated
       const userCookie = {
         active_key: userDetails.active_key,
         id: userDetails.id,
@@ -158,7 +162,7 @@ const JobDetail = ({
         additional_info: userDetails.additional_info,
         is_email_verify: true,
         notice_period_id: userDetails.notice_period_id,
-        is_profile_completed: userDetails.is_profile_completed,
+        is_profile_completed: userDetails.is_profile_completed
       }
 
       setCookie('user', userCookie)
@@ -167,17 +171,17 @@ const JobDetail = ({
   }
 
   const handleApplyJob = () => {
-    if (!userCookie) { 
+    if (!userCookie) {
       setQuickApplyModalShow(true)
     } else {
       if (userCookie && !userCookie.is_email_verify) {
         handleVerifyEmailClick()
       }
-      
+
       if (selectedJob?.external_apply_url) {
         addExternalJobClickService(selectedJob?.id)
       }
-      
+
       window.open(applyJobLink)
     }
   }
@@ -188,7 +192,7 @@ const JobDetail = ({
       router.push(applyJobLink)
     }
   }
-  
+
   const isCategoryApplied = category === 'applied'
   const isCategorySaved = category === 'saved'
   const publicJobUrl = isCategoryApplied
@@ -203,7 +207,7 @@ const JobDetail = ({
   }
 
   const handleShowReportJob = () => {
-    if ( authCookie && userCookie) {
+    if (authCookie && userCookie) {
       setIsShowReportJob(true)
     } else {
       setCookie('isReportJob', true)
@@ -224,40 +228,38 @@ const JobDetail = ({
     <React.Fragment>
       <div className={styles.JobDetail}>
         <div className={classNamesCombined([styles.JobDetailContent])}>
-          <div className={classNamesCombined([isStickyClass, styles.JobDetailHeader])} ref={detailHeaderRef}>
+          <div
+            className={classNamesCombined([isStickyClass, styles.JobDetailHeader])}
+            ref={detailHeaderRef}
+          >
             <div>
-              <div
-                className={styles.JobDetailOptionImage}
-              >
+              <div className={styles.JobDetailOptionImage}>
                 <Dropdown>
-                    {selectedJob?.status_key === 'active' && (
-                      <>
-                        <Link to={publicJobUrl} external className={styles.JobDetailOptionItem}>
-                          <Text textStyle='lg'>View in new tab</Text>
-                        </Link>
-                        <div
-                          className={styles.JobDetailOptionItem}
-                          onClick={handleShowReportJob}
-                        >
-                          <Text textStyle='lg'>Report job</Text>
-                        </div>
-                      </>
-                    )}
-                    {isCategoryApplied && !checkHasApplicationWithdrawn() && (
-                      <div
-                        className={styles.JobDetailOptionItem}
-                        onClick={() => setIsShowModalWithdrawApplication(true)}
-                      >
-                        <Text textStyle='lg'>Withdraw Application</Text>
+                  {selectedJob?.status_key === 'active' && (
+                    <>
+                      <Link to={publicJobUrl} external className={styles.JobDetailOptionItem}>
+                        <Text textStyle='lg'>View in new tab</Text>
+                      </Link>
+                      <div className={styles.JobDetailOptionItem} onClick={handleShowReportJob}>
+                        <Text textStyle='lg'>Report job</Text>
                       </div>
-                    )}
-
+                    </>
+                  )}
+                  {isCategoryApplied && !checkHasApplicationWithdrawn() && (
                     <div
                       className={styles.JobDetailOptionItem}
-                      onClick={() => setIsShowModalShare(true)}
+                      onClick={() => setIsShowModalWithdrawApplication(true)}
                     >
-                      <Text textStyle='lg'>Share this job</Text>
+                      <Text textStyle='lg'>Withdraw Application</Text>
                     </div>
+                  )}
+
+                  <div
+                    className={styles.JobDetailOptionItem}
+                    onClick={() => setIsShowModalShare(true)}
+                  >
+                    <Text textStyle='lg'>Share this job</Text>
+                  </div>
                 </Dropdown>
               </div>
 
@@ -268,10 +270,7 @@ const JobDetail = ({
               style={{ backgroundImage: `url(${selectedJob?.company?.logo})` }}
             />
             <div className={styles.JobDetailInfo}>
-              <Link
-                to={publicJobUrl}
-                external
-              >
+              <Link to={publicJobUrl} external>
                 <Text textStyle='lg' bold className={styles.JobDetailTitle}>
                   {selectedJob?.job_title}
                 </Text>
@@ -285,58 +284,54 @@ const JobDetail = ({
               {selectedJob?.is_urgent && <JobTag tag='Urgent' tagType='urgent' />}
               <JobTag tag={selectedJob?.job_type_value} />
               <div className={styles.JobDetailButtonsWrapper}>
-                  {!isCategoryApplied && (
-                    <div className={styles.JobDetailButtons}>
-                      {selectedJob?.status_key === 'active' && (
-                        <>
-                          {!selectedJob?.is_applied ? (
-                            <MaterialButton
-                              variant='contained'
-                              capitalize
-                              onClick={handleApplyJob}
-                            >
-                              <Text textColor='white' bold>
-                                Apply Now
-                              </Text>
-                            </MaterialButton>
-                          ) : (
-                            <MaterialButton variant='contained' capitalize disabled>
-                              <Text textColor='white' bold>
-                                Applied
-                              </Text>
-                            </MaterialButton>
-                          )}
-                        </>
-                      )}
-                      <MaterialButton
-                        variant='outlined'
-                        capitalize
-                        isLoading={!userCookie && isSaveClicked}
-                        onClick={() => {
-                          if (userCookie) {
-                            if (!isCategorySaved && !isSavedJob) {
-                              handlePostSaveJob({ jobId: selectedJob?.id })
-                              setIsSavedJob(true)
-                            }
-
-                            if (isSavedJob) {
-                              handleDeleteSavedJob({ jobId: selectedJob?.id })
-                              setIsSavedJob(false)
-                            }
+                {!isCategoryApplied && (
+                  <div className={styles.JobDetailButtons}>
+                    {selectedJob?.status_key === 'active' && (
+                      <>
+                        {!selectedJob?.is_applied ? (
+                          <MaterialButton variant='contained' capitalize onClick={handleApplyJob}>
+                            <Text textColor='white' bold>
+                              Apply Now
+                            </Text>
+                          </MaterialButton>
+                        ) : (
+                          <MaterialButton variant='contained' capitalize disabled>
+                            <Text textColor='white' bold>
+                              Applied
+                            </Text>
+                          </MaterialButton>
+                        )}
+                      </>
+                    )}
+                    <MaterialButton
+                      variant='outlined'
+                      capitalize
+                      isLoading={!userCookie && isSaveClicked}
+                      onClick={() => {
+                        if (userCookie) {
+                          if (!isCategorySaved && !isSavedJob) {
+                            handlePostSaveJob({ jobId: selectedJob?.id })
+                            setIsSavedJob(true)
                           }
 
-                          if (!userCookie) {
-                            setIsSaveClicked(true)
-                            router.push('/login/jobseeker?redirect=/jobs-hiring/job-search')
+                          if (isSavedJob) {
+                            handleDeleteSavedJob({ jobId: selectedJob?.id })
+                            setIsSavedJob(false)
                           }
-                        }}
-                      >
-                        <Text textColor='primaryBlue' bold>
-                          {isSavedJob || isCategorySaved ? 'Saved' : 'Save Job'}
-                        </Text>
-                      </MaterialButton>
-                    </div>
-                  )}
+                        }
+
+                        if (!userCookie) {
+                          setIsSaveClicked(true)
+                          router.push('/login/jobseeker?redirect=/jobs-hiring/job-search')
+                        }
+                      }}
+                    >
+                      <Text textColor='primaryBlue' bold>
+                        {isSavedJob || isCategorySaved ? 'Saved' : 'Save Job'}
+                      </Text>
+                    </MaterialButton>
+                  </div>
+                )}
 
                 {selectedJob?.status_key !== 'active' && (
                   <Text textStyle='base' className={styles.JobDetailStatus}>
@@ -344,16 +339,19 @@ const JobDetail = ({
                     <span>This job is no longer hiring</span>
                   </Text>
                 )}
-                
+
                 {(!isCategoryApplied || !isCategorySaved) && (
                   <Text textStyle='sm' textColor='darkgrey' className={styles.JobDetailPostedAt}>
-                    Posted on {selectedJob?.refreshed_at}
+                    Applied on {selectedJob?.refreshed_at}
                   </Text>
                 )}
               </div>
             </div>
           </div>
-          <div className={classNamesCombined([styles.JobDetailBody, isStickyClass])} style={{ top: detailHeaderHeight }}>
+          <div
+            className={classNamesCombined([styles.JobDetailBody, isStickyClass])}
+            style={{ top: detailHeaderHeight }}
+          >
             <div className={styles.JobDetailPref}>
               <ul className={styles.JobDetailPrefList}>
                 <li className={styles.JobDetailPrefItem}>
@@ -433,7 +431,7 @@ const JobDetail = ({
               <div
                 className={classNamesCombined([
                   styles.JobDetailSectionBody,
-                  styles.JobDetailDescriptionSectionBody,
+                  styles.JobDetailDescriptionSectionBody
                 ])}
                 dangerouslySetInnerHTML={{ __html: selectedJob?.job_description_html }}
               />
@@ -445,7 +443,7 @@ const JobDetail = ({
               <div
                 className={classNamesCombined([
                   styles.JobDetailSectionBody,
-                  styles.JobDetailRequirementSectionBody,
+                  styles.JobDetailRequirementSectionBody
                 ])}
                 dangerouslySetInnerHTML={{ __html: selectedJob?.job_requirements_html }}
               />
@@ -516,42 +514,42 @@ const JobDetail = ({
               )}
             </div>
             {selectedJob?.recruiter && (
-            <div className={styles.JobDetailRecruiter}>
-              <Text textStyle='xl' bold>
-                Connect directly to recruiter after applying
-              </Text>
-              <div className={styles.JobDetailRecruiterInfo}>
-                <div
-                  className={styles.JobDetailRecruiterInfoImage}
-                  style={{
-                    backgroundImage: `url(${selectedJob?.recruiter.avatar || DefaultAvatar})`,
-                  }}
-                />
-                <div className={styles.JobDetailRecruiterInfoText}>
-                  <div className={styles.JobDetailRecruiterName}>
-                    <Text textStyle='lg' bold>
-                      {selectedJob?.recruiter.full_name},{' '}
-                    </Text>
-                    <Text textStyle='lg'>
-                      &nbsp;{selectedJob?.recruiter.work_experience.job_title}
-                    </Text>
-                  </div>
-                  <div className={styles.JobDetailRecruiterContent}>
-                    <Text textStyle='lg' textColor='darkgrey'>
-                      <img src={RateIcon} height='14' width='15' />
-                      {selectedJob?.recruiter.response_rate}% response rate, responds{' '}
-                      {selectedJob?.recruiter.response_time}
-                    </Text>
-                    <Text textStyle='lg' textColor='darkgrey'>
-                      <img src={LocationPinIcon} height='14' width='15' />
-                      Last active on{' '}
-                      {moment(selectedJob?.recruiter.last_active_at).format('MM/DD/YYYY')}
-                    </Text>
+              <div className={styles.JobDetailRecruiter}>
+                <Text textStyle='xl' bold>
+                  Connect directly to recruiter after applying
+                </Text>
+                <div className={styles.JobDetailRecruiterInfo}>
+                  <div
+                    className={styles.JobDetailRecruiterInfoImage}
+                    style={{
+                      backgroundImage: `url(${selectedJob?.recruiter.avatar || DefaultAvatar})`
+                    }}
+                  />
+                  <div className={styles.JobDetailRecruiterInfoText}>
+                    <div className={styles.JobDetailRecruiterName}>
+                      <Text textStyle='lg' bold>
+                        {selectedJob?.recruiter.full_name},{' '}
+                      </Text>
+                      <Text textStyle='lg'>
+                        &nbsp;{selectedJob?.recruiter.work_experience.job_title}
+                      </Text>
+                    </div>
+                    <div className={styles.JobDetailRecruiterContent}>
+                      <Text textStyle='lg' textColor='darkgrey'>
+                        <img src={RateIcon} height='14' width='15' />
+                        {selectedJob?.recruiter.response_rate}% response rate, responds{' '}
+                        {selectedJob?.recruiter.response_time}
+                      </Text>
+                      <Text textStyle='lg' textColor='darkgrey'>
+                        <img src={LocationPinIcon} height='14' width='15' />
+                        Last active on{' '}
+                        {moment(selectedJob?.recruiter.last_active_at).format('MM/DD/YYYY')}
+                      </Text>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
             <div className={styles.aboutCompany}>
               <Text bold textStyle='lg' className={styles.aboutCompanyHeader}>
                 About the company
@@ -571,18 +569,22 @@ const JobDetail = ({
         </div>
       </div>
 
-      {quickApplyModalShow && <QuickApplyModal
-        jobDetails={selectedJob}
-        modalShow={quickApplyModalShow}
-        handleModalShow={setQuickApplyModalShow}
-        config={config}
-      />}
+      {quickApplyModalShow && (
+        <QuickApplyModal
+          jobDetails={selectedJob}
+          modalShow={quickApplyModalShow}
+          handleModalShow={setQuickApplyModalShow}
+          config={config}
+        />
+      )}
 
-      {isShowModal && <ModalVerifyEmail
-        email={userCookie ? userCookie.email : ''}
-        isShowModal={isShowModal}
-        handleModal={handleCloseModal}
-      />}
+      {isShowModal && (
+        <ModalVerifyEmail
+          email={userCookie ? userCookie.email : ''}
+          isShowModal={isShowModal}
+          handleModal={handleCloseModal}
+        />
+      )}
     </React.Fragment>
   )
 }
