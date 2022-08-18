fd056487eb8f5d1e0358956d06f9d710081add4b
developerkit87 <kitsiang@bossjob.com>
2022-08-18 15:06:19 +0800

    Update: Remove unused codes

--- helpers/formValidation.js
+++ helpers/formValidation.js
@@ -1,11 +1,3 @@
-// export const urlValidation = (value) => {
-//   if (value && (value.includes('https://https://'))) {
-//     return 'Please enter a valid URL.'
-//   } else {
-//     return null
-//   }
-// }
-
 export const urlValidation = (value) => {
   const urlExpression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
   const urlRegex = new RegExp(urlExpression)
