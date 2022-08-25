7e2d9a8c6ad1eb298f768c73150ace5ba84655f4
cctipmao <cctip_mao@proton.me>
2022-08-25 18:47:31 +0800

    fix:lint

--- components/JobSearchFilters/JobSearchFilters.tsx
+++ components/JobSearchFilters/JobSearchFilters.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import * as ReactDOM from 'react-dom'
 import { flatMap } from 'lodash-es'
 /* Vendor */
