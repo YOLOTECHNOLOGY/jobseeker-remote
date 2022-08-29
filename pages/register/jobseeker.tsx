f7e8f9f694554fbf2c9f3776d5fe41b5aee77295
cctipmao <cctip_mao@proton.me>
2022-08-29 17:09:56 +0800

    feature:separate register page's hooks

--- pages/register/jobseeker.tsx
+++ pages/register/jobseeker.tsx
@@ -1,17 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { useDispatch, useSelector } from 'react-redux'
+import React, {  } from 'react'
 
 import { Visibility, VisibilityOff } from '@mui/icons-material'
 import InputAdornment from '@mui/material/InputAdornment'
 import IconButton from '@mui/material/IconButton'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
-import { useForm } from 'react-hook-form'
 
 /* Redux Actions */
-import { socialLoginRequest } from 'store/actions/auth/socialLogin'
-import { registerJobseekerRequest } from 'store/actions/auth/registerJobseeker'
 
 /* Components */
 import MaterialButton from 'components/MaterialButton'
@@ -25,109 +20,22 @@ import SocialMediaAuth from 'components/SocialMediaAuth/SocialMediaAuth'
 /* Styles */
 import styles from './Register.module.scss'
 import MetaText from '../../components/MetaText'
-import Link from '../../components/Link'
+import useRegister from 'pages/hooks/useRegister'
 
 const Register = () => {
-  const router = useRouter()
-  const dispatch = useDispatch()
-
-  const [firstName, setFirstName] = useState('')
-  const [firstNameError, setFirstNameError] = useState(null)
-  const [lastName, setLastName] = useState('')
-  const [lastNameError, setLastNameError] = useState(null)
-  const [email, setEmail] = useState('')
-  const [emailError, setEmailError] = useState(null)
-
-  const [password, setPassword] = useState('')
-  const [isSubscribe, setIsSubscribe] = useState(true)
-  const [showPassword, setShowPassword] = useState(false)
-  const [passwordError, setPasswordError] = useState(null)
-
   const {
+    firstName, setFirstName, 
+    firstNameError, lastName, setLastName, 
+    lastNameError, email, setEmail, 
+    emailError, password, setPassword, 
+    passwordError, showPassword, isSubscribe, setIsSubscribe,
+    errors,
     register,
-    formState: { errors },
-  } = useForm()
-
-  const isRegisteringJobseeker = useSelector((store: any) => store.auth.registerJobseeker.fetching)
-
-  const registerJobseekerState = useSelector((store: any) => store.auth.registerJobseeker)
-
-  const handleOnShowPassword = () => setShowPassword(!showPassword)
-
-  useEffect(() => {
-    if (firstName) {
-      setFirstNameError(null)
-    }
-  }, [firstName])
-
-  useEffect(() => {
-    if (lastName) {
-      setLastNameError(null)
-    }
-  }, [lastName])
-
-  useEffect(() => {
-    let emailErrorMessage = null
-
-    if (email && !/\S+@\S+\.\S+/.test(email)) {
-      emailErrorMessage = 'Please enter a valid email address.'
-    }
-
-    setEmailError(emailErrorMessage)
-
-  }, [email])
-
-  useEffect(() => {
-    let passwordErrorMessage = null
-
-    if (password?.length > 0 && password?.length < 8) {
-      passwordErrorMessage = 'Please enter a longer password(minimum of 8 characters)'
-    } else if (password?.length > 16) {
-      passwordErrorMessage = 'Please enter a shorter password(maximum of 16 characters)'
-    } else {
-      passwordErrorMessage = null
-    }
-    setPasswordError(passwordErrorMessage)
-  }, [password])
-
-  useEffect(() => {
-    if (registerJobseekerState.error === 'The email has already been taken.') {
-      setEmailError(<p>A user with this email address already exists. Please enter a different email address or <Link to='/login/jobseeker' className='default'>log in</Link>.</p>)
-    }
-  
-  }, [registerJobseekerState])
-
-  const handleRegister = () => {
-    if (!firstName) {
-      setFirstNameError('Please enter your first name.')
-    }
-
-    if (!lastName) {
-      setLastNameError('Please enter your last name.')
-    }
-
-    if (!email) {
-      setEmailError('Please enter your email address.')
-    }
-
-    if (!password) {
-      setPasswordError('Please enter a longer password(minimum of 8 characters)')
-    }
-
-    if (firstName && lastName && email && password && !firstNameError && !lastNameError && !emailError && !passwordError) {
-      const payload = {
-        email,
-        password,
-        first_name: firstName,
-        last_name: lastName,
-        terms_and_condition: false,
-        is_subscribe: isSubscribe,
-        redirect: router.query?.redirect || null
-      }
-
-      dispatch(registerJobseekerRequest({ ...payload }))
-    }
-  }
+    handleRegister,
+    isRegisteringJobseeker,
+    handleOnShowPassword,
+    callbackRequest,
+  } = useRegister()
 
   const errorText = (errorMessage: string) => {
     return (
@@ -136,11 +44,6 @@ const Register = () => {
       </Text>
     )
   }
-
-  const callbackRequest = (payload) => {
-    dispatch(socialLoginRequest(payload))
-  }
-
   return (
     <AuthLayout
       headingText={
