dedeab4a2187ff2891176ab06300b7688686a1e5
BossJob-Lee <cctip_lee@proton.me>
2022-09-13 18:22:37 +0800

    Update: accessSetting UI init

--- components/AccountSettings/FieldFormWrapper.tsx
+++ components/AccountSettings/FieldFormWrapper.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 /** Vendors */
 import classNames from 'classnames'
@@ -6,6 +6,7 @@ import classNames from 'classnames'
 /** Component */
 import Text from 'components/Text'
 import { AccountSettingEditIconPen, AccountSettingDeleteIconBin } from 'images'
+import { Button } from '@mui/material'
 
 // styles
 import styles from './FieldFormWrapper.module.scss'
@@ -17,10 +18,13 @@ const FieldFormWrapper = ({
   style,
   sideNode,
   titleInlineBlock,
-  ...rest
+  setEdit,
+  btnSuccessText
 }: any) => {
+  const [isShowSubmtButton, setIsShowSubmtButton] = useState(false)
+
   return (
-    <div className={classNames([className, styles.fieldFromWrapper])} style={style} {...rest}>
+    <div className={classNames([className, styles.fieldFromWrapper])} style={style}>
       <div className={styles.fieldFromWrapper_title}>
         <Text
           tagName='h2'
@@ -34,14 +38,39 @@ const FieldFormWrapper = ({
           {label}
         </Text>
         <div className={styles.fieldFromWrapper_title_edit_icon}>
-          <img src={AccountSettingEditIconPen} alt='account setting edit icon pen' />
+          <img
+            onClick={() => {
+              setIsShowSubmtButton(true)
+              setEdit(label)
+            }}
+            src={AccountSettingEditIconPen}
+            alt='account setting edit icon pen'
+          />
           <img src={AccountSettingDeleteIconBin} alt='account setting eelete icon bin' />
         </div>
       </div>
       {sideNode}
-      <div style={{ padding: '10px 0 2em', marginBottom: '1.25em' }}>{children}</div>
+      <div style={{ padding: '10px 0 0' }}>
+        {children}
+        {isShowSubmtButton && (
+          <div className={styles.fieldFromWrapper_button}>
+            <Button variant='contained' disableElevation>
+              {btnSuccessText ? btnSuccessText : 'Send OTP'}
+            </Button>
+            <Button
+              variant='outlined'
+              onClick={() => {
+                setEdit(null)
+                setIsShowSubmtButton(false)
+              }}
+            >
+              Cancel
+            </Button>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
 
-export default FieldFormWrapper
+export default React.memo(FieldFormWrapper)
--- pages/dashboard/profile/settings/index.tsx
+++ pages/dashboard/profile/settings/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { wrapper } from 'store'
 import { END } from 'redux-saga'
 
@@ -6,17 +6,25 @@ import Layout from 'components/Layout'
 import Text from 'components/Text'
 import FieldFormWrapper from 'components/AccountSettings/FieldFormWrapper'
 import MaterialTextField from 'components/MaterialTextField'
+import MaterialBasicSelect from 'components/MaterialBasicSelect'
 
 // mui
 import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
+import Switch from '@mui/material/Switch'
 import { ThemeProvider, createTheme } from '@mui/material'
+
+// tools
+import { getSmsCountryList } from 'helpers/jobPayloadFormatter'
 
 // actions
 import { fetchUserOwnDetailRequest } from 'store/actions/users/fetchUserOwnDetail'
+import { fetchConfigRequest } from 'store/actions/config/fetchConfig'
 
 // styles
 import styles from './settings.module.scss'
+import { blue } from '@mui/material/colors'
+import { handleNumericInput } from 'helpers/handleInput'
 
 interface TabPanelProps {
   children?: React.ReactNode
@@ -39,7 +47,6 @@ function TabPanel(props: TabPanelProps) {
     </div>
   )
 }
-
 function a11yProps(index: number) {
   return {
     id: `simple-tab-${index}`,
@@ -47,9 +54,60 @@ function a11yProps(index: number) {
   }
 }
 
-const AccountSettings = ({ userOwnDetail }: any) => {
+const AccountSettings = ({ userOwnDetail, config }: any) => {
   console.log(userOwnDetail)
-  const [value, setValue] = React.useState(0)
+  const smsCountryList = getSmsCountryList(config)
+
+  const [value, setValue] = useState(0)
+  const [edit, setEdit] = useState(null)
+
+  const getSmsCountryCode = (phoneNumber, smsCountryList) => {
+    if (!phoneNumber || !smsCountryList) return null
+
+    const matchedCountryCode = smsCountryList.filter((country) => {
+      return phoneNumber.includes(country.value)
+    })
+
+    return matchedCountryCode ? matchedCountryCode[0]?.value : null
+  }
+  const [smsCode, setSmsCode] = useState(
+    getSmsCountryCode(userOwnDetail?.phone_num, smsCountryList) || '+63'
+  )
+
+  const [email, setEmail] = useState(userOwnDetail.email ? userOwnDetail.email : null)
+  const [emailError, setEmailError] = useState(null)
+
+  const [phoneNum, setPhoneNum] = useState(userOwnDetail.phone_num ? userOwnDetail.phone_num : null)
+  const [phoneNumError] = useState(null)
+
+  const [password, setPassword] = useState({
+    currentPassword: '',
+    newPassword: null,
+    confirmNewPassword: null
+  })
+  const [passwordError] = useState(null)
+
+  const [emailSubscribe] = useState({
+    notifications: false,
+    newNotifications: false,
+    careerHiringNewNotifications: false
+  })
+
+  useEffect(() => {
+    let errorText = null
+    if (email && !/\S+@\S+\.\S+/.test(email)) {
+      errorText = 'Please enter a valid email address.'
+    }
+
+    setEmailError(errorText)
+  }, [email])
+
+  useEffect(() => {
+    // const errorText = null
+    // if (!/\D/.test(phoneNum)) {
+    //   errorText =
+    // }
+  }, [phoneNum])
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue)
@@ -73,6 +131,29 @@ const AccountSettings = ({ userOwnDetail }: any) => {
       }
     }
   })
+
+  const accountTheme = createTheme({
+    palette: {
+      primary: blue
+    }
+  })
+
+  const errorText = (errorMessage: string) => {
+    return (
+      <Text textStyle='sm' textColor='red' tagName='p' className={styles.fieldError}>
+        {errorMessage}
+      </Text>
+    )
+  }
+
+  const requiredLabel = (text: string) => {
+    return (
+      <>
+        <span>{text}</span>
+        <span className={styles.stepFieldRequired}>*</span>
+      </>
+    )
+  }
 
   return (
     <Layout>
@@ -100,31 +181,198 @@ const AccountSettings = ({ userOwnDetail }: any) => {
 
         <div className={styles.accessSettingsContainer}>
           <TabPanel value={value} index={0}>
-            <FieldFormWrapper label='Email'>
-              {!userOwnDetail.is_email_verify ? (
-                <div className={styles.formWrapper}>
-                  <Text className={styles.bottomSpacing}>{userOwnDetail.email}</Text>
-                </div>
-              ) : (
-                <div>
-                  <Text>To receive job applications update, please verify your email.</Text>
-                  <div>
-                    <MaterialTextField
-                      className={styles.RegisterFormInput}
-                      id='email'
-                      label='Email Address'
-                      variant='outlined'
-                      size='small'
-                      defaultValue='000000'
-                      autoComplete='off'
-                    />
+            <ThemeProvider theme={accountTheme}>
+              <FieldFormWrapper label='Email' setEdit={setEdit}>
+                {edit === 'Email' ? (
+                  <div className={styles.accessSettingsContainer_fromWrapper}>
+                    <Text>To receive job applications update, please verify your email.</Text>
+                    <div className={styles.accessSettingsContainer_fromWrapper_edit}>
+                      <MaterialTextField
+                        className={styles.accessSettingsContainer_fromWrapper_edit_input}
+                        id='email'
+                        label='Email Address'
+                        variant='outlined'
+                        value={email}
+                        size='small'
+                        defaultValue={email}
+                        autoComplete='off'
+                        error={emailError ? true : false}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      {emailError && errorText(emailError)}
+                    </div>
                   </div>
+                ) : (
+                  <div className={styles.formWrapper}>
+                    <Text className={styles.bottomSpacing}>{email}</Text>
+                  </div>
+                )}
+              </FieldFormWrapper>
+
+              {/*  mobile number  */}
+              <FieldFormWrapper label='Mobile Number' setEdit={setEdit}>
+                {edit === 'Mobile Number' ? (
+                  <div className={styles.accessSettingsContainer_fromWrapper}>
+                    <Text>To receive job applications update, please verify your email.</Text>
+                    <div className={styles.accessSettingsContainer_fromWrapper_edit}>
+                      <div className={styles.accessSettingsContainer_fromWrapper_edit_wrapper}>
+                        <div
+                          className={styles.accessSettingsContainer_fromWrapper_edit_wrapper_block}
+                        >
+                          <MaterialBasicSelect
+                            className={styles.accessSettingsContainer_fromWrapper_edit_input}
+                            label='Country'
+                            value={smsCode}
+                            options={smsCountryList}
+                            onChange={(e) => {
+                              setSmsCode(e.target.value)
+                            }}
+                          />
+                          {phoneNumError && errorText(phoneNumError)}
+                        </div>
+
+                        <div
+                          className={styles.accessSettingsContainer_fromWrapper_edit_wrapper_block}
+                        >
+                          <MaterialTextField
+                            className={styles.accessSettingsContainer_fromWrapper_edit_input}
+                            label={requiredLabel('Contact Number')}
+                            size='small'
+                            error={phoneNumError ? true : false}
+                            value={phoneNum}
+                            onChange={(e) => setPhoneNum(handleNumericInput(e.target.value))}
+                          />
+                          {phoneNumError && errorText(phoneNumError)}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className={styles.formWrapper}>
+                    <Text className={styles.bottomSpacing}>{phoneNum}</Text>
+                  </div>
+                )}
+              </FieldFormWrapper>
+
+              {/* password */}
+              <FieldFormWrapper label='Password' setEdit={setEdit}>
+                {edit === 'Password' ? (
+                  <div className={styles.accessSettingsContainer_fromWrapper}>
+                    <Text>To receive job applications update, please verify your email.</Text>
+                    <div className={styles.accessSettingsContainer_fromWrapper_edit}>
+                      <div>
+                        <MaterialTextField
+                          className={styles.accessSettingsContainer_fromWrapper_edit_input}
+                          label={requiredLabel('Current password')}
+                          size='small'
+                          error={passwordError ? true : false}
+                          value={password.currentPassword}
+                          onChange={(e) =>
+                            setPassword((params) => ({
+                              ...params,
+                              currentPassword: e.target.value
+                            }))
+                          }
+                          type='password'
+                        />
+                        {passwordError && errorText(passwordError)}
+
+                        <MaterialTextField
+                          className={styles.accessSettingsContainer_fromWrapper_edit_input}
+                          label={requiredLabel('New password')}
+                          size='small'
+                          error={passwordError ? true : false}
+                          value={password.currentPassword}
+                          onChange={(e) =>
+                            setPassword((params) => ({
+                              ...params,
+                              currentPassword: e.target.value
+                            }))
+                          }
+                          type='password'
+                        />
+                        {passwordError && errorText(passwordError)}
+
+                        <MaterialTextField
+                          className={styles.accessSettingsContainer_fromWrapper_edit_input}
+                          label={requiredLabel('Confirm new password')}
+                          size='small'
+                          error={passwordError ? true : false}
+                          value={password.currentPassword}
+                          onChange={(e) =>
+                            setPassword((params) => ({
+                              ...params,
+                              currentPassword: e.target.value
+                            }))
+                          }
+                          type='password'
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className={styles.formWrapper}>
+                    <Text className={styles.bottomSpacing}>***********</Text>
+                  </div>
+                )}
+              </FieldFormWrapper>
+
+              {/*  Email Notification  */}
+              <FieldFormWrapper label='Email Notification' setEdit={setEdit}>
+                <div className={styles.accessSettingsContainer_swtich}>
+                  <Text>Receive system notifications:</Text>
+                  <Switch
+                    defaultChecked
+                    checked={emailSubscribe.careerHiringNewNotifications}
+                    // onChange={(ev) => {}}
+                  />
                 </div>
-              )}
-            </FieldFormWrapper>
+                <div className={styles.accessSettingsContainer_swtich}>
+                  <Text>Receive new chat notifications:</Text>
+                  <Switch
+                    defaultChecked
+                    // onChange={(ev) => {}}
+                  />
+                </div>
+                <div className={styles.accessSettingsContainer_swtich}>
+                  <Text>Receive career and hiring tips newsletter:</Text>
+                  <Switch
+                    defaultChecked
+                    // onChange={(ev) => {}}
+                  />
+                </div>
+              </FieldFormWrapper>
+
+              <FieldFormWrapper label='Linked Accounts' setEdit={setEdit}>
+                <div className={styles.accessSettingsContainer_swtich}>
+                  <Text>Facebook Messenger</Text>
+                  <Switch
+                    defaultChecked
+                    // onChange={(ev) => {}}
+                  />
+                </div>
+              </FieldFormWrapper>
+
+              <FieldFormWrapper label='Credit Card' setEdit={setEdit}></FieldFormWrapper>
+            </ThemeProvider>
           </TabPanel>
+
+          {/* Job Alert */}
           <TabPanel value={value} index={1}>
-            Item Two
+            <div className={styles.JobAlertContainer}>
+              <Text tagName='h3'>Job alert</Text>
+              <FieldFormWrapper label='All jobs' setEdit={setEdit} btnSuccessText='Save'>
+                <Text block className={styles.JobAlertContainer_title}>
+                  All location
+                </Text>
+                <Text block className={styles.JobAlertContainer_desc}>
+                  Filters: No active filters selected
+                </Text>
+                <Text block className={styles.JobAlertContainer_desc}>
+                  Frequency: Daily
+                </Text>
+              </FieldFormWrapper>
+            </div>
           </TabPanel>
         </div>
       </div>
@@ -135,13 +383,25 @@ const AccountSettings = ({ userOwnDetail }: any) => {
 export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ query, req }) => {
   const accessToken = req.cookies?.accessToken ? req.cookies.accessToken : null
 
+  if (!accessToken) {
+    return {
+      redirect: {
+        destination: '/login/jobseeker?redirect=/dashboard/profile/settings',
+        permanent: false
+      }
+    }
+  }
+
+  store.dispatch(fetchConfigRequest())
   store.dispatch(fetchUserOwnDetailRequest({ accessToken }))
   store.dispatch(END)
   await (store as any).sagaTask.toPromise()
   const storeState = store.getState()
+  const config = storeState.config.config.response
   const userOwnDetail = storeState.users.fetchUserOwnDetail.response
   return {
     props: {
+      config,
       accessToken,
       userOwnDetail
     }
