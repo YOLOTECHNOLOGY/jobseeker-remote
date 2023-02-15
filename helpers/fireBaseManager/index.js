cda38166880fa3442391edf9cdd6dea0d3525eb9
cctipmao <cctip_mao@proton.me>
2023-02-15 15:32:13 +0800

    fix: app page

--- helpers/fireBaseManager/index.js
+++ helpers/fireBaseManager/index.js
@@ -1,39 +1,46 @@
 import { initializeApp } from 'firebase/app';
-import { getMessaging, onMessage, getToken } from "firebase/messaging";
+import { getMessaging, onMessage, getToken, isSupported } from "firebase/messaging";
 const vapidKey = 'BPSNbeeP647k3y02pPIWBgh8qiEUywYXa0aY9WbZ_yGO6beOY8oah_CyL9Q1mojbtzdX4NJpVI83w149n0yyh7Y'
 export const initFireBase = () => {
-    try {
-        const firebaseConfig = {
-            apiKey: "AIzaSyDcvw3JBG8PPTQsppvPdkn39378fSBtF9g",
-            authDomain: "bossjob2021-6b1d7.firebaseapp.com",
-            projectId: "bossjob2021-6b1d7",
-            storageBucket: "bossjob2021-6b1d7.appspot.com",
-            messagingSenderId: "640891885977",
-            appId: "1:640891885977:web:5c45a6a3c1b8d24eeadb08",
-            measurementId: "G-V6JBNRJMFL"
-        };
-        const app = initializeApp(firebaseConfig)
-        console.log({ app })
-        const messaging = getMessaging(app)
-        console.log({ messaging })
-        
-        getToken(messaging, { vapidKey })
-            .then(token => {
-                console.log({ token })
-                onMessage(messaging, (payload) => {
-                    console.log('Message received. ', payload);
-                    // ...
+    isSupported().then(supported => {
+        if (!supported) {
+            alert('Notifications from bossjob is not supported on this browser!')
+            return
+        }
+        try {
+            const firebaseConfig = {
+                apiKey: "AIzaSyDcvw3JBG8PPTQsppvPdkn39378fSBtF9g",
+                authDomain: "bossjob2021-6b1d7.firebaseapp.com",
+                projectId: "bossjob2021-6b1d7",
+                storageBucket: "bossjob2021-6b1d7.appspot.com",
+                messagingSenderId: "640891885977",
+                appId: "1:640891885977:web:5c45a6a3c1b8d24eeadb08",
+                measurementId: "G-V6JBNRJMFL"
+            };
+            const app = initializeApp(firebaseConfig)
+            console.log({ app })
+            const messaging = getMessaging(app)
+            console.log({ messaging })
+
+            getToken(messaging, { vapidKey })
+                .then(token => {
+                    console.log({ token })
+                    onMessage(messaging, (payload) => {
+                        console.log('Message received. ', payload);
+                        // ...
+                    });
+                }).catch(e => {
+                    console.log('getTokenError', e)
                 });
-            }).catch(e => {
-                console.log('getTokenError', e)
-            });
-        Notification.requestPermission().then((permission) => {
-            console.log({ permission })
-            if (permission === 'granted') {
-                console.log('Notification permission granted.');
-            }
-        })
-    } catch (e) {
-        console.log({ firebaseError: e })
-    }
+            Notification.requestPermission().then((permission) => {
+                console.log({ permission })
+                if (permission === 'granted') {
+                    console.log('Notification permission granted.');
+                }
+            }).catch(e => console.log(e))
+        } catch (e) {
+            console.log({ firebaseError: e })
+        }
+    })
+
 }
