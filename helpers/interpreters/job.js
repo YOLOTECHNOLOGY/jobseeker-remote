a2d98be55109d77aef736477fc4cbb65e100cb5d
cctipmao <cctip_mao@proton.me>
2023-03-03 19:13:19 +0800

    fix: job position

--- helpers/interpreters/job.js
+++ helpers/interpreters/job.js
@@ -4,9 +4,10 @@ import { M } from 'imforbossjob'
 
 export default command => command.cata({
     redirectJobPage: jobId => M
-        (context => Promise.resolve().then(() => {
-            const router = context.getRouter()
-            router.push(`/job/${jobId}`)
+        (() => Promise.resolve().then(() => {
+            // const router = context.getRouter()
+            // router.push(`/job/${jobId}`)
+            window.open(location.host + `/job/${jobId}`)
         })),
 
 
