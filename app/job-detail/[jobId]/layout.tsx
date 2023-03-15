32c3c1e7d08d1004871f8ee6df6f2cec31bb8662
BossJob-Lee <cctip_lee@proton.me>
2023-03-15 19:40:18 +0800

    fix: revise ad

---
+++ app/job-detail/[jobId]/layout.tsx
@@ -0,0 +1,9 @@
+import Footer from 'components/Footer'
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <section>{children}</section>
+      <Footer />
+    </>
+  )
+}
---
+++ app/job-detail/[jobId]/loading.tsx
@@ -0,0 +1,6 @@
+import styles from './page.module.scss'
+const Loading = () => {
+  return <div className={styles.container}>Loading .....假装这里是骨架屏</div>
+}
+
+export default Loading
