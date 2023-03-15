dae00f77ebd6855dafcd058cd95ff7b71752d106
BossJob-Lee <cctip_lee@proton.me>
2023-03-15 13:58:04 +0800

    feat: job detail

---
+++ app/job-detail/[jobId]/components/Desc/Desc.tsx
@@ -0,0 +1,5 @@
+const Desc = () => {
+  return <div>Job Description</div>
+}
+
+export default Desc
---
+++ app/job-detail/[jobId]/components/Head/Head.tsx
@@ -0,0 +1,21 @@
+const Head = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur lore <div className='salary'>₱75 - 80K</div>
+      </h1>
+      <div>Makati |⟩Degree | 3 to 5 years experience | Full-time</div>
+      <div>
+        <div>
+          <button>Save</button>
+          <button>Chat now</button>
+        </div>
+
+        <div>Fill up resume online</div>
+        <div>Upload resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Head
