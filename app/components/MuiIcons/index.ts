baf7d5c95423a02aa5901b431efdbfadd5e0ddda
BossJob-Lee <cctip_lee@proton.me>
2023-03-20 17:42:03 +0800

    feat: job detail

---
+++ app/components/MuiIcons/index.ts
@@ -0,0 +1,5 @@
+'use client'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+
+export { FavoriteBorderIcon, FavoriteIcon }
