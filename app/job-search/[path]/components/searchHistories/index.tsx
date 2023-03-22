8260cafa3831284e1f58786f8a013871509abc12
cctipmao <cctip_mao@proton.me>
2023-03-22 14:41:20 +0800

    fix: qr code

---
+++ app/job-search/[path]/components/searchHistories/index.tsx
@@ -0,0 +1,21 @@
+import { buildQuery } from 'app/main-page/helper'
+import React from 'react'
+import styles from './index.module.scss'
+import Link from 'next/link'
+const SearchHistories = (props: any) => {
+    const { list = [], location } = props
+    return <div className={styles.container}>
+        <div className={styles.title}>Related Search</div>
+        {list.map(item => {
+            return <Link
+                key={item}
+                href={buildQuery(location, item)}
+                prefetch={true}
+            >
+                <div className={styles.item}>{item}</div>
+            </Link>
+        })}
+    </div>
+}
+
+export default SearchHistories
