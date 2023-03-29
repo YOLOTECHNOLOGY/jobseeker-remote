98ba0e07b596b81a93f40233a2176fde4f9c6e66
BossJob-Lee <cctip_lee@proton.me>
2023-03-29 17:31:51 +0800

    fix: page scroll top 0

--- app/jobs-hiring/[path]/components/Pagination/index.tsx
+++ app/jobs-hiring/[path]/components/Pagination/index.tsx
@@ -4,18 +4,22 @@ import { Pagination as MUIPagination } from 'app/components/MUIs'
 import { useSearchParams, usePathname } from 'next/navigation'
 import { LoadingContext } from 'app/components/providers/loadingProvider'
 const Pagination = (props: any) => {
-    const { count, page, pageQueryKey = 'page' } = props
-    const searchParams = useSearchParams()
-    const pathname = usePathname()
-    const { push } = useContext(LoadingContext)
-    return <MUIPagination
-        // style={{position:'sticky',bottom:0,left:0}}
-        count={count}
-        page={page}
-        onChange={(e, page) => {
-            const newSearchParams = new URLSearchParams(searchParams.toString())
-            newSearchParams.set(pageQueryKey, '' + page)
-            push(pathname + '?' + newSearchParams.toString())
-        }} />
+  const { count, page, pageQueryKey = 'page' } = props
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { push } = useContext(LoadingContext)
+  return (
+    <MUIPagination
+      // style={{position:'sticky',bottom:0,left:0}}
+      count={count}
+      page={page}
+      onChange={(e, page) => {
+        const newSearchParams = new URLSearchParams(searchParams.toString())
+        newSearchParams.set(pageQueryKey, '' + page)
+        push(pathname + '?' + newSearchParams.toString())
+        scrollTo(0, 0)
+      }}
+    />
+  )
 }
 export default Pagination
