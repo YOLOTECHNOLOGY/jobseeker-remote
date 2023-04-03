0635536c098f0e70a479a5e398005e4c939929e2
cctipmao <cctip_mao@proton.me>
2023-04-03 15:53:57 +0800

    fix: seo

--- app/my-jobs/layout.tsx
+++ app/my-jobs/layout.tsx
@@ -1,5 +1,11 @@
 // import styles from './index.module.scss'
 
+const seoMetaTitle = `My Jobs`
+const description = 'Bossjob - Career Platform for Professionals in Philippines'
+export const  metadata = {
+  title: seoMetaTitle,
+  description: description,
+}
 export default function RootLayout({
     children,
   }: {
