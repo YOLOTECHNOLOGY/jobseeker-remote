a146d90e19df3af7b7e4c14dd1e77f8d36908b89
cctipmao <cctip_mao@proton.me>
2023-04-12 14:06:03 +0800

    fix: env

--- helpers/country.js
+++ helpers/country.js
@@ -1,6 +1,6 @@
 export const getCountryKey = () => {
         const path = typeof window === 'undefined'? process.env.NEXT_PUBLIC_HOST_PATH : window.location.href
-        if(path.includes('.sg')){
+        if(path?.includes?.('.sg')){
             return 'sg'
         } else {
             return 'ph'
