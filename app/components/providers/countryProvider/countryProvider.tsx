d09d4365b6ed4156b39702aa1d3070899039dc33
cctipmao <cctip_mao@proton.me>
2023-04-12 13:49:40 +0800

    fix: provider

--- app/components/providers/countryProvider/countryProvider.tsx
+++ app/components/providers/countryProvider/countryProvider.tsx
@@ -1,14 +1,9 @@
 'use client'
-import React, { createContext, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchConfigRequest } from 'store/actions/config/fetchConfig';
+import React, { createContext } from 'react';
 
 export const CountryContext = createContext({ countryKey: '' })
 const CountryProvider = ({ children,value }: any) => {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(fetchConfigRequest())
-    }, [])
+   
     return <CountryContext.Provider value={value}>
         {children}
     </CountryContext.Provider>
