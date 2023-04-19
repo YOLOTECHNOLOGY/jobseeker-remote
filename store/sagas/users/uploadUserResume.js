b8d3f859615392774cc073676a7cf28147739634
BossJob-Lee <cctip_lee@proton.me>
2023-04-19 19:57:52 +0800

    fix: upload resume

--- store/sagas/users/uploadUserResume.js
+++ store/sagas/users/uploadUserResume.js
@@ -1,9 +1,9 @@
-import {call, put, takeLatest } from 'redux-saga/effects'
+import { call, put, takeLatest } from 'redux-saga/effects'
 import { push } from 'connected-next-router'
 import { UPLOAD_USER_RESUME_REQUEST } from 'store/types/users/uploadUserResume'
 import {
   uploadUserResumeSuccess,
-  uploadUserResumeFailed,
+  uploadUserResumeFailed
 } from 'store/actions/users/uploadUserResume'
 import { uploadUserResumeService } from 'store/services/users/uploadUserResume'
 
