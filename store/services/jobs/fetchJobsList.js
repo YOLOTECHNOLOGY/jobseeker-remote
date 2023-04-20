5bd9aa15ba22bb068420dbb789ec1e2222b9f0fe
BossJob-Lee <cctip_lee@proton.me>
2023-04-20 16:47:32 +0800

    fix: jobs/filter update jobs/search

--- store/services/jobs/fetchJobsList.js
+++ store/services/jobs/fetchJobsList.js
@@ -1,12 +1,15 @@
 import queryString from 'query-string'
 import configuredAxios from 'helpers/configuredAxios'
 
-const fetchJobsListService = (payload, accessToken=null) => {
+const fetchJobsListService = (payload, accessToken = null) => {
   const endpointType = accessToken ? 'protected' : 'public'
 
   const axios = configuredAxios('job', endpointType, false, accessToken)
-  
-  return axios.get(`filter?${queryString.stringify(payload)}`)
+  // TODO 不应该这一写，暂时做一下简单处理之后会改
+  if (!payload.is_company_verified) {
+    payload.is_company_verified = null
+  }
+  return axios.get(`search?${queryString.stringify(payload)}`)
 }
 
 export { fetchJobsListService }
