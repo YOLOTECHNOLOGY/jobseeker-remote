99c4a435ee792006964f9c7fcbc2d8f6ccfd5f97
cctipmao <cctip_mao@proton.me>
2023-04-21 18:32:39 +0800

    fix: scss for job

--- app/job/[jobId]/layout.tsx
+++ app/job/[jobId]/layout.tsx
@@ -1,4 +1,8 @@
 import Footer from 'components/Footer'
+import './page.module.scss'
+import 'components/Header/Header.module.scss'
+import 'app/globals.scss'
+// import 'app/globals.scss'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
