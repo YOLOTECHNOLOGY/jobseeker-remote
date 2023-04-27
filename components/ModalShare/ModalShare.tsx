ec5ce05e4ee0d655b59c4a031e73af440e3dc8a1
cctipmao <cctip_mao@proton.me>
2023-04-27 18:26:28 +0800

    fix: job location

--- components/ModalShare/ModalShare.tsx
+++ components/ModalShare/ModalShare.tsx
@@ -39,7 +39,7 @@ const ModalShare = ({
 
   const meInfo = useSelector((store: any) => store.users.fetchUserOwnDetail.response)
 
-  jobDetailUrl = `${process.env.NEXT_PUBLIC_HOST_PATH}${jobDetailUrl}`
+  jobDetailUrl = `${location.origin}${jobDetailUrl}`
 
   useEffect(() => {
     if (meInfo.id) {
