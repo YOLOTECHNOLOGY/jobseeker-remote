f0a6e8ca4aa8910bead969ec5bc3a155b022b6e1
cctipmao <cctip_mao@proton.me>
2023-04-27 16:38:31 +0800

    fix: hot fix similar jobs

--- store/services/jobs/fetchSimilarJobs.js
+++ store/services/jobs/fetchSimilarJobs.js
@@ -2,7 +2,7 @@ import configuredAxios from 'helpers/configuredAxios'
 
 const fetchSimilarJobsService = (payload) => {
   const axios = configuredAxios('job', 'protected')
-  return axios.get(`/similar?size=${payload.size}&&job_id=${payload.jobId}`)
+  return axios.get(`/similar?size=${payload.size}&job_id=${payload.jobId}`)
 }
 
 export { fetchSimilarJobsService }
