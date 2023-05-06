6b2d9bb5cdb48001969c340f84a69fc0929b115b
cctipmao <cctip_mao@proton.me>
2023-05-06 12:16:40 +0800

    fix: timeout

---
+++ store/sagas/notificationBar/index.js
@@ -0,0 +1,19 @@
+import { put, takeEvery, delay } from 'redux-saga/effects'
+import { closeNotification } from 'store/actions/notificationBar/notificationBar'
+import { DISPLAY_NOTIFICATION } from 'store/types/notificationBar/notificationBar'
+
+let refCount = 0
+function* autoDisappearNotification() {
+    refCount++
+    yield delay(3000)
+    refCount--
+    console.log({ refCount })
+    if (refCount === 0) {
+        yield put(closeNotification())
+    }
+
+}
+
+export default function* notificationBar() {
+    yield takeEvery(DISPLAY_NOTIFICATION, autoDisappearNotification)
+}
