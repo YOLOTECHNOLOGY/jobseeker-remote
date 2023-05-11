49cda8692f174391a150cf218c0f2a3e742c41ad
bossjobmatt <bossjob_matt@proton.me>
2023-05-11 14:36:22 +0800

    fix: format date time

--- components/Chat/interview/interviewDetail/index.tsx
+++ components/Chat/interview/interviewDetail/index.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react'
 import { useDispatch } from 'react-redux'
 import { displayNotification } from 'store/actions/notificationBar/notificationBar'
 import styles from './index.module.scss'
-
+import moment from 'moment'
 import { CopyIconHaveTextCopy } from 'images'
 
 const InterviewDetail = (props: any) => {
@@ -13,7 +13,7 @@ const InterviewDetail = (props: any) => {
       ...[
         ['Job Title', data?.job_title],
         ['Location', data?.address],
-        ['Date,Time', data.interviewed_at],
+        ['Date,Time', moment(data.interviewed_at).format('DD MMM YYYY dddd, HH:mm A')],
         ['Contact person', data?.contact_person],
         ['Contact number', data?.contact_person_contact_num],
         ['Video link', data?.video_link],
