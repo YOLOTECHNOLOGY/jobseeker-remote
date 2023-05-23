2fbded4372fb4527a1d8183f97b45fc4864db5fc
cctipmao <cctip_mao@proton.me>
2023-05-23 17:59:18 +0800

    fix: layout error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,2 @@
-/* eslint-disable react/display-name */
-import React from 'react'
-import Layout from './[lang]/layout'
-import { getCountryAndLang } from '../middleware'
-import { cookies } from 'next/headers'
 
-export default (props: any) => {
-    const locale = getCountryAndLang(cookies() as any || 'en-US')
-    const lang = locale?.[1]
-    const withLangProps = { ...props, params: { lang } }
-    {/* @ts-expect-error Async Server Component */ }
-    return <Layout {...withLangProps} />
-}
+export default ({ children }: any) => children
