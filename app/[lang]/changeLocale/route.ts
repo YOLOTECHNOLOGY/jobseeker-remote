337472a828c8c15f0cb100b1d01a84ac46f20c88
Taylor Zhao <bossjob_taylor@proton.me>
2023-05-26 11:49:48 +0800

    fix: enhance the switch country logic

--- app/[lang]/changeLocale/route.ts
+++ app/[lang]/changeLocale/route.ts
@@ -1,8 +1,9 @@
-import { cookies } from 'next/headers'
 
 import { getCountryId, getLanguageId } from 'helpers/country'
 
 import { fetchUserSetting } from 'store/services/swtichCountry/userSetting'
+import { NextResponse } from 'next/server'
+import { refreshToken as refreshTokenKey, accessToken as accessTokenKey, userKey } from 'helpers/cookies'
 
 async function removeServiceCache(token, lang?) {
   try {
@@ -19,25 +20,43 @@ async function removeServiceCache(token, lang?) {
   }
 
 }
+const isTest = false; // only true when develop at local
+const localAddress = isTest ? ['http://local.dev.bossjob.ph:3005', 'http://local.dev.bossjob.sg:3005'] : []
 
 export async function GET(request) {
-
-  const accessToken = new URL(request.url).searchParams.get('accessToken')
+  const params = new URL(request.url).searchParams
+  const accessToken = params.get(accessTokenKey)
+  const refreshToken = params.get(refreshTokenKey)
+  const country = params.get('country')
+  const user = params.get(userKey)
   const { url } = request
   const lang = url.split('//')[1].split('/')[1]
-  await removeServiceCache(accessToken, lang)
+  if (accessToken) {
+    await removeServiceCache(accessToken, lang)
+  }
+  const newUrl = localAddress.find(url => url.includes(country)) || process.env.NEXT_PUBLIC_HOST_PATH
 
-  return new Response(null, {
+  const response = new NextResponse(null, {
     status: 301,
     headers: {
-      'Set-Cookie': `accessToken=${accessToken};path=/`,
-      Location: process.env.NEXT_PUBLIC_HOST_PATH,
+      "Access-Control-Allow-Headers": "Set-Cookie",
+      Location: `${newUrl}/${lang}`,
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS'
-      // "Access-Control-Allow-Headers": "Set-Cookie"
+      'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
     }
   })
+
+  // set cookies with token in header
+  // the maxAge unit is second
+  const maxTime = 60 * 60 * 24 // a day
+  response.cookies.set(accessTokenKey, accessToken, { path: '/', maxAge: accessToken ? maxTime : 0 })
+  response.cookies.set(refreshTokenKey, `${refreshToken}`, { path: '/', maxAge: refreshToken ? maxTime : 0 })
+  // clear the user data, and refetch the user data again
+  response.cookies.set(userKey, user, { path: '/', maxAge: user ? maxTime : 0 })
+  response.cookies.set('location', '', { path: '/', maxAge: 0 })
+
+  return response
 }
 
 // export async function OPTIONS() {
