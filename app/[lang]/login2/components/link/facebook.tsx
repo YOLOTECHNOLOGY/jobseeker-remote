b9e3e383e75bf704d8b38d43884d237df48cce0e
bossjobmatt <bossjob_matt@proton.me>
2023-05-30 11:56:16 +0800

    feat: add google

--- app/[lang]/login2/components/link/facebook.tsx
+++ app/[lang]/login2/components/link/facebook.tsx
@@ -1,11 +1,85 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+
+import { jobbseekersSocialLoginRequest } from 'store/actions/auth/jobseekersSocialLogin'
+import { FacebookLogo } from 'images'
+
 import styles from '../../index.module.scss'
 
-export default function FacebookLogin() {
+interface IFacebook {
+  className?: string
+  activeKey?: number
+  isLogin?: boolean
+  callBackMethod?: Function
+  redirect?: string | string[]
+  loading?: boolean
+}
+
+const FacebookLogin = (props: IFacebook) => {
+  const {activeKey,isLogin,redirect} = props
+
+  const dispatch = useDispatch()
+
+
+  const callBackMethod = (payload) => {
+    const data = {
+      ...payload,
+      // ...router.query,
+      // avatar: payload.pictureUrl ? payload.pictureUrl : '',
+      email: payload.email ? payload.email : '',
+      social_user_token: payload.accessToken,
+      social_type: payload.socialType,
+      social_user_id: payload.userId,
+      source: 'web'
+    }
+    if (payload.pictureUrl) {
+      data.avatar = payload.pictureUrl
+    }
+    dispatch(jobbseekersSocialLoginRequest(data))
+  }
+
+
+  const handleAuthClick = () => {
+    let accessToken
+
+    window.FB.login(
+      function (response) {
+        if (response.authResponse) {
+          accessToken = response.authResponse.accessToken
+
+          window.FB.api('/me?fields=id,first_name,last_name,email', function (response) {
+            const payload = {
+              userId: response.id,
+              firstName: response.first_name,
+              lastName: response.last_name,
+              email: response.email,
+              pictureUrl: response.profile_pic,
+              accessToken: accessToken,
+              socialType: 'fb',
+              redirect: redirect,
+              activeKey: activeKey ? activeKey : null,
+              isLogin: isLogin ? true : false
+            }
+
+            callBackMethod(payload)
+            // eslint-disable-next-line no-console
+          })
+        } else {
+          // User cancelled login or did not fully authorize
+        }
+      },
+      {
+        scope: 'email'
+      }
+    )
+  }
+
   return (
     <div className={styles.login_item}>
-        <img src='https://dev.bossjob.ph/_next/image?url=https%3A%2F%2Fdev-assets.bossjob.com%2Fcompanies%2F31430%2Flogo%2Flogo.png&w=48&q=75'></img>
-        <span>Continue with Facebook</span>
+        <img src={FacebookLogo}></img>
+        <span onClick={handleAuthClick}>Continue with Facebook</span>
     </div>
   )
 }
+
+export default FacebookLogin
--- app/[lang]/login2/components/link/google.tsx
+++ app/[lang]/login2/components/link/google.tsx
@@ -1,11 +1,121 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+
+import { jobbseekersSocialLoginRequest } from 'store/actions/auth/jobseekersSocialLogin'
+import { GoogleLogo } from 'images'
+
 import styles from '../../index.module.scss'
 
-export default function GoogleLogin() {
+interface IGoogle {
+  className?: string,
+  activeKey?: number,
+  isLogin?: boolean,
+  callBackMethod?: Function,
+  redirect?: string | string[],
+}
+
+const GoogleLogin = (props: IGoogle)  => {
+  const {activeKey,isLogin,redirect} = props
+  const [googleAuth, setGoogleAuth] = useState(null)
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setGoogleAuth(null)
+      const handleClientLoad = () => window.gapi.load('client:auth2', initClient)
+      const initClient = () => {
+        window.gapi.client
+          .init({
+            apiKey: 'AIzaSyAFYSp8vzxmXF_PourfSFW6t0VynH5d9Vs',
+            clientId: '197019623682-n8mch4vlad6r9c6t3vhovu01sartbahq.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/userinfo.profile',
+          })
+          .then(() => {
+            setGoogleAuth(window.gapi.auth2.getAuthInstance())
+          })
+      }
+
+       const script = document.createElement('script')
+
+       script.src = 'https://apis.google.com/js/api.js'
+       script.async = true
+       script.defer = true
+       script.onload = handleClientLoad
+
+       document.body.appendChild(script)
+
+       return () => {
+         document.body.removeChild(script)
+       }
+    }
+  }, [])
+
+  const handleAuthClick = () => {
+    googleAuth?.signIn().then(() => {
+      handleSigninStatus()
+    })
+  }
+
+
+  const callBackMethod = (payload) => {
+    const data = {
+      ...payload,
+      // ...router.query,
+      // avatar: payload.pictureUrl ? payload.pictureUrl : '',
+      email: payload.email ? payload.email : '',
+      social_user_token: payload.accessToken,
+      social_type: payload.socialType,
+      social_user_id: payload.userId,
+      source: 'web'
+    }
+    if (payload.pictureUrl) {
+      data.avatar = payload.pictureUrl
+    }
+    dispatch(jobbseekersSocialLoginRequest(data))
+  }
+
+
+  const handleSigninStatus = async () => {
+    const user = googleAuth.currentUser.get()
+    const isAuthorized = user.hasGrantedScopes('profile')
+
+    if (isAuthorized) {
+      const accessToken = user.getAuthResponse().id_token
+
+      if (typeof window !== 'undefined') {
+        const request = window.gapi.client.request({
+          method: 'GET',
+          path:
+            'https://www.googleapis.com/oauth2/v2/userinfo?fields=id,email,family_name,given_name,picture'
+        })
+
+        // Execute the API request.
+        request.execute(function(response) {
+          const payload = {
+            userId: response.id,
+            firstName: response.given_name,
+            lastName: response.family_name,
+            email: response.email,
+            pictureUrl: response.picture,
+            accessToken: accessToken,
+            socialType: 'google',
+            redirect: redirect,
+            activeKey: activeKey,
+            isLogin: isLogin ? true : false
+          }
+          callBackMethod(payload)
+        })
+      }
+    }
+  }
+
   return (
     <div className={styles.login_item}>
-        <img src='https://dev.bossjob.ph/_next/image?url=https%3A%2F%2Fdev-assets.bossjob.com%2Fcompanies%2F31430%2Flogo%2Flogo.png&w=48&q=75'></img>
-        <span>Continue with Google</span>
+        <img src={GoogleLogo} />
+        <span onClick={handleAuthClick}>Continue with Google</span>
     </div>
   )
 }
+
+export default GoogleLogin
