de33b94f0aa0a5fa6979b7c0134129a95f1982d5
bossjobmatt <bossjob_matt@proton.me>
2023-05-31 16:54:42 +0800

    feat: update apple login

--- app/[lang]/login2/components/link/apple.tsx
+++ app/[lang]/login2/components/link/apple.tsx
@@ -6,6 +6,7 @@ import { removeItem } from 'helpers/localStorage'
 import useGetStartedClient from '../../hooks/useGetStarted'
 import { useSearchParams } from 'next/navigation'
 import { useDispatch, useSelector } from 'react-redux'
+import * as jose from 'jose'
 
 interface IApple {
   isLogin?: boolean
@@ -43,6 +44,7 @@ const AppleLogin = (props: IApple) => {
     usePopup: true
   }
 
+  const rs = jose.decodeJwt('eyJraWQiOiJXNldjT0tCIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLmJvc3Nqb2Iud2ViIiwiZXhwIjoxNjg1NjA4NDEzLCJpYXQiOjE2ODU1MjIwMTMsInN1YiI6IjAwMTE2Mi5jODllNmY5YzQ3MjA0M2MxYjg1OWVmNWFiNDRhNjI4OC4wNzM4IiwiY19oYXNoIjoiTkNxN0ZkaFpLSUlWV010T3BzN1c3USIsImVtYWlsIjoiNjR0N2Q2eG00bUBwcml2YXRlcmVsYXkuYXBwbGVpZC5jb20iLCJlbWFpbF92ZXJpZmllZCI6InRydWUiLCJpc19wcml2YXRlX2VtYWlsIjoidHJ1ZSIsImF1dGhfdGltZSI6MTY4NTUyMjAxMywibm9uY2Vfc3VwcG9ydGVkIjp0cnVlfQ.uorwivnykV5rVBNZvgb4hEEXjc3v3u-Xzdk6OKO3rYi3D3Oa-hafJqwEzAxtHVeGJSK3d8oulpBUPN_StKFJh-fwcJ80Govp57DYJ0rUR7TkdAkUawAFUqZA0IgbMGB6HNpKgoM4-T0nE4y0qatANtGUE7YXN9x9O-WhBSpsVDxZzBpc0LjqKXrjugkwzl5X9nM2pTPQFt6bmo0fuslFV43bKSTPnG0Rr2pLeHQBq06TZ0WUgPd4N_5HCl58grLqMxXw4octZmK6OeacCG4B5jo93qLYQlMzpT6OspBZ9BTgEWKjbIvpar5fOUZ-vhU_594E-Zk1vyRFiFAQnMYuCA')
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const handleClientLoad = () => {
@@ -76,28 +78,28 @@ const AppleLogin = (props: IApple) => {
     try {
       const data = await window.AppleID.auth.signIn()
       // Handle successful response.
-      console.log('success', data)
       callBackMethod(data)
     } catch (error) {
-      // Handle error.
       console.log('error', error)
     }
   }
 
   const callBackMethod = (payload) => {
-    const data = {
-      ...payload,
-      ...query,
-      email: payload?.user?.email ? payload?.user?.email : '',
-      social_user_token: payload.authorization.id_token,
-      social_type: 'apple',
-      social_user_id: payload?.userId || '',
-      source: 'web'
+    try {
+      const decodeJwt = jose.decodeJwt(payload?.authorization?.id_token)
+      const data = {
+        ...payload,
+        ...query,
+        email: payload?.user?.email ? payload?.user?.email : '',
+        social_user_token: payload?.authorization?.id_token,
+        social_type: 'apple',
+        social_user_id: decodeJwt.sub || '',
+        source: 'web'
+      }
+      dispatch(jobbseekersSocialLoginRequest(data))
+    } catch (error) {
+      console.log('error', error)
     }
-    if (payload?.pictureUrl) {
-      data.avatar = payload.pictureUrl
-    }
-    dispatch(jobbseekersSocialLoginRequest(data))
   }
 
   return (
