8a287c2f97813b097c8475b47c43201b9eff06da
bossjobmatt <bossjob_matt@proton.me>
2023-06-01 10:39:18 +0800

    feat: update login css

--- app/[lang]/login2/components/link/email.tsx
+++ app/[lang]/login2/components/link/email.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Link from 'next/link'
-const EmailLink = ()=>{
 
+import { EmailIcon } from 'images'
+import { usePathname } from "next/navigation";
+
+const EmailLink = ()=>{
+  const pathname = usePathname()
+  
   return (
     <li>
-     <Link href="/login2/email">
-     <img src='https://dev.bossjob.ph/_next/image?url=https%3A%2F%2Fdev-assets.bossjob.com%2Fcompanies%2F31430%2Flogo%2Flogo.png&w=48&q=75'></img>
+     <Link href={`${pathname}/email`}>
+     <img src={EmailIcon}></img>
       Email
     </Link>
     </li>
--- app/[lang]/login2/components/link/phone.tsx
+++ app/[lang]/login2/components/link/phone.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Link from 'next/link'
+
+import { PhoneIcon } from 'images'
+import { usePathname } from "next/navigation";
+
 const PhoneLink = ()=>{
+  const pathname = usePathname()
 
   return (
     <li>
-     <Link href="/login2/phone">
-     <img src='https://dev.bossjob.ph/_next/image?url=https%3A%2F%2Fdev-assets.bossjob.com%2Fcompanies%2F31430%2Flogo%2Flogo.png&w=48&q=75'></img>
+     <Link href={`${pathname}/phone`}>
+     <img src={PhoneIcon}></img>
       Phone
     </Link>
     </li>
