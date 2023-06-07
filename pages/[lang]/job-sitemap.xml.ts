a32b81baa381e55141ff6af7446d3f7ad2ab903e
BossJob-Lee <cctip_lee@proton.me>
2023-06-07 13:47:30 +0800

    fix: update sitemap

--- pages/[lang]/job-sitemap.xml.ts
+++ pages/[lang]/job-sitemap.xml.ts
@@ -1,14 +1,16 @@
 import axios from 'axios'
+import { getCountryKey } from 'helpers/country'
 
 const JobSitemap = () => {
   return
 }
 
 export const getServerSideProps = async ({ res }) => {
+  const country = getCountryKey()
   const response = await axios.get(
     process.env.ENV === 'production'
-      ? 'https://assets.bossjob.com/job-sitemap.xml'
-      : 'https://dev-assets.bossjob.com/job-sitemap.xml'
+      ? `https://assets.bossjob.com/job-sitemap-${country}.xml`
+      : `https://dev-assets.bossjob.com/job-sitemap-${country}.xml`
   )
 
   res.setHeader('Content-Type', 'text/xml')
@@ -16,7 +18,7 @@ export const getServerSideProps = async ({ res }) => {
   res.end()
 
   return {
-    props: {},
+    props: {}
   }
 }
 
