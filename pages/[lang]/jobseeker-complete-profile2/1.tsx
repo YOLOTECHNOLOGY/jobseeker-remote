0644729bad9790e0f9332294e00d3de88211fe1b
lvchangyu <bossjob_mayday@proto.me>
2023-06-16 15:06:30 +0800

    feat: add loginDialog

--- pages/[lang]/jobseeker-complete-profile2/1.tsx
+++ pages/[lang]/jobseeker-complete-profile2/1.tsx
@@ -613,7 +613,7 @@ export const getServerSideProps = wrapper.getServerSideProps((store) => async ({
   if (!accessToken) {
     return {
       redirect: {
-        destination: `/${query.lang}/get-started?redirect=/jobseeker-complete-profile/1`,
+        destination: `/${query.lang}/get-started?redirect=/jobseeker-complete-profile`,
         permanent: false,
         lang
       }
