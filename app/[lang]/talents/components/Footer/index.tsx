47482ae0a7dfed4bac7bfb98091c5e45e73efc08
victor <victor.lee1180@proton.me>
2023-06-19 17:34:43 +0800

    feat: add seo

--- app/[lang]/talents/components/Footer/index.tsx
+++ app/[lang]/talents/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from 'next/link';
 import style from './index.module.scss';
 import {getCountryKey, getLang} from 'helpers/country'
--- app/[lang]/talents/layout.tsx
+++ app/[lang]/talents/layout.tsx
@@ -1,35 +1,67 @@
-'use client'
 
 // import './flexible';
 import Footer from './components/Footer'
-import useResponsiveFont from "./components/responseHook";
 import './index.module.scss'
-export default function ShopLayout(props:{
-    children: React.ReactNode;
-    params: {
-      tag: string;
-      item: string;
-    }
-  } ) {
-    // URL -> /shop/shoes/nike-air-max-97
-    // `params` -> { tag: 'shoes', item: 'nike-air-max-97' }
-    useResponsiveFont(360, 540);
-    return (
-        <section style={{
-          width: '100%',
-          // overflow: "hidden",
-          overflowX: 'clip',
-          minHeight: '100vh',
-          backgroundColor:'#ffffff',
+import Head from "next/head";
+import {Metadata} from "next"
 
-        }}>
-            {/* <header> */}
-            {/*     <h1>boosJob</h1> */}
-            {/* </header> */}
-            <main>
-            {props.children}
-            </main>
-            <Footer/>
-        </section>
-    )
-  }
+import {useContext} from "react";
+import {languageContext} from "../components/providers/languageProvider";
+import {getCountryKey} from "../../../helpers/country";
+import {formatTemplateString} from "../../../helpers/formatter";
+import {getDictionary} from "../../../get-dictionary";
+
+// eslint-disable-next-line valid-jsdoc
+/**
+ * Generate metadata for the page
+ * @doc https://nextjs.org/docs/api-reference/data-fetching/getInitialProps
+ */
+export async function generateMetadata(
+	props:{params: { lang: any }}
+): Promise<Metadata> {
+	// read route params
+	const dictionary = await getDictionary(props.params.lang);
+	const country = dictionary.seo[getCountryKey()];
+	const description = dictionary.seo?.landing?.description;
+	const regex = /\{\{([^}]+)\}\}/g;
+	const final_description = description.replace(regex, (_, match) => {
+		return match.toLowerCase() === 'country' ? country : match;
+	});
+	return {
+		title: dictionary.seo?.landing?.title,
+		description: final_description,
+	}
+}
+
+export default function ShopLayout(props: {
+	children: React.ReactNode;
+	params: {
+		tag: string;
+		item: string;
+	}
+}) {
+	// URL -> /shop/shoes/nike-air-max-97
+	// `params` -> { tag: 'shoes', item: 'nike-air-max-97' }
+	return (
+		<>
+
+			<section style={{
+				width: '100%',
+				// overflow: "hidden",
+				overflowX: 'clip',
+				minHeight: '100vh',
+				backgroundColor: '#ffffff',
+
+			}}>
+				{/* <header> */}
+				{/*     <h1>boosJob</h1> */}
+				{/* </header> */}
+				<main>
+					{props.children}
+				</main>
+				<Footer/>
+			</section>
+		</>
+
+	)
+}
