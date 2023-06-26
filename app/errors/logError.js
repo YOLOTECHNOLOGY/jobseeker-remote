8e8df7b602afe1e0e9bd485d1e6f7730d275dc6d
cctipmao <cctip_mao@proton.me>
2023-06-27 01:11:50 +0800

    fix: cancel log server info

--- app/errors/logError.js
+++ app/errors/logError.js
@@ -1,29 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from 'axios'
 const logError = error => {
-    if (process.env.NODE_ENV !== 'production') {
-        return
-    }
-    if (typeof window !== 'undefined') {
-        axios.post(window.location.origin + '/errors/report', { error })
-    } else {
-        const log4js = require('log4js')
-        const logger = log4js.getLogger('serverError')
-        logger.level = 'error'
-        logger.error(error)
-    }
+    // if (process.env.NODE_ENV !== 'production') {
+    //     return
+    // }
+    // if (typeof window !== 'undefined') {
+    //     axios.post(window.location.origin + '/errors/report', { error })
+    // } else {
+    //     const log4js = require('log4js')
+    //     const logger = log4js.getLogger('serverError')
+    //     logger.level = 'error'
+    //     logger.error(error)
+    // }
 }
 export const logServerInfo = info => {
-    if (process.env.NODE_ENV !== 'production') {
-        return
-    }
-    if (typeof window !== 'undefined') {
-        return
-    } else {
-        const log4js = require('log4js')
+    // if (process.env.NODE_ENV !== 'production') {
+    //     return
+    // }
+    // if (typeof window !== 'undefined') {
+    //     return
+    // } else {
+    //     const log4js = require('log4js')
 
-        const logger = log4js.getLogger('serverInfo')
-        logger.level = 'info'
-        logger.info(info)
-    }
+    //     const logger = log4js.getLogger('serverInfo')
+    //     logger.level = 'info'
+    //     logger.info(info)
+    // }
 }
 export default logError
