37d8bd70427a91b42f9fce31577d5f956a497bda
Taylor Zhao <bossjob_taylor@proton.me>
2023-06-26 11:34:40 +0800

    fix:  add defaultSEO data for no-lang page

--- app/(main-page)/(no-lang)/layout.tsx
+++ app/(main-page)/(no-lang)/layout.tsx
@@ -1,24 +1,30 @@
-import PublicLayout from "app/components/publicLayout"
-import { getDictionary } from "get-dictionary"
-import { getCountryKey, getLang } from "helpers/country"
-import { formatTemplateString } from "helpers/formatter"
+import PublicLayout from 'app/components/publicLayout'
+import { getDictionary } from 'get-dictionary'
+import { getCountryKey, getLang } from 'helpers/country'
+import { formatTemplateString } from 'helpers/formatter'
 
-export default async function RootLayout(props: any) {
-
-  const { children } = props
+export async function generateMetadata() {
   const lang = getLang()
   const dictionary = await getDictionary(lang)
+
   const defaultSEO = {
     title: formatTemplateString(dictionary.seo.siteName, dictionary.seo[getCountryKey()]),
     description: formatTemplateString(dictionary.seo.siteName, dictionary.seo[getCountryKey()]),
     imageUrl: 'https://assets.bossjob.com/website/OGTagImage.png',
     canonical: ''
   }
+
+  return defaultSEO
+}
+
+export default async function RootLayout(props: any) {
+  const { children } = props
+  const defaultSEO = await generateMetadata()
+
   return (
     /* @ts-expect-error Async Server Component */
-    <PublicLayout {...props} seo={defaultSEO} >
+    <PublicLayout {...props} seo={defaultSEO}>
       {children}
     </PublicLayout>
-
   )
 }
