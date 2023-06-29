2af3100d099602f45ec74eba99a2b6534d415393
lvchangyu <bossjob_mayday@proto.me>
2023-06-29 19:33:16 +0800

    fix: tost

--- app/components/Toast/index.tsx
+++ app/components/Toast/index.tsx
@@ -1,28 +1,27 @@
-import ToastItem from "./ToastItem";
-import ReactDOM from "react-dom";
+import ToastItem from './toastItem'
+import ReactDOM from 'react-dom'
 
-const createDom = (content,duration,type)=>{
-  const dom = document.createElement('div');
-  const JSXdom = (<ToastItem content={content} duration={duration} type={type}/>);
-    ReactDOM.render(JSXdom,dom)
-  document.body.appendChild(dom);
+const createDom = (content, duration, type) => {
+  const dom = document.createElement('div')
+  const JSXdom = <ToastItem content={content} duration={duration} type={type} />
+  ReactDOM.render(JSXdom, dom)
+  document.body.appendChild(dom)
 }
 
-
 const Toast = {
-    dom: null,
-    success(content: string, duration?: number) {
-      createDom(content,duration,'success')   
-    },
-    error(content: string, duration?: number) {
-      createDom(content,duration,'error')
-    },
-    warning(content: string, duration?: number) {
-      createDom(content,duration,'warning')
-    },
-    info(content: string, duration?: number) {
-      createDom(content,duration,'info')
-    }
-};
+  dom: null,
+  success(content: string, duration?: number) {
+    createDom(content, duration, 'success')
+  },
+  error(content: string, duration?: number) {
+    createDom(content, duration, 'error')
+  },
+  warning(content: string, duration?: number) {
+    createDom(content, duration, 'warning')
+  },
+  info(content: string, duration?: number) {
+    createDom(content, duration, 'info')
+  }
+}
 
-export default Toast;
+export default Toast
