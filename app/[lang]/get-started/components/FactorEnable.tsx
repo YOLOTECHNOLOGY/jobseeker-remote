4ed1ac53e6d5f72e156b0cecd5a8f2969290e4a5
lvchangyu <bossjob_mayday@proto.me>
2023-07-14 16:11:35 +0800

    feat: update login ui

--- app/[lang]/get-started/components/FactorEnable.tsx
+++ app/[lang]/get-started/components/FactorEnable.tsx
@@ -1,43 +1,39 @@
-import React, { useEffect,useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from '../index.module.scss'
 import useGetStarted from '../hooks/useGetStarted'
-import lottie from "lottie-web";
+import lottie from 'lottie-web'
 function FactorEnable(props: any) {
- // const userInfo = useSelector((store: any) => store.auth.jobseekersLogin.response)
+  // const userInfo = useSelector((store: any) => store.auth.jobseekersLogin.response)
   const { newGetStarted } = props.lang
-  const { defaultLoginCallBack ,userInfo} = useGetStarted()
-  const container= useRef(null);
+  const { defaultLoginCallBack, userInfo } = useGetStarted()
+  const containerIMg = useRef(null)
   console.log({ userInfo })
   useEffect(() => {
-    setTimeout(() => {
-      defaultLoginCallBack(userInfo)
-    }, 3000)
+    // setTimeout(() => {
+    //   defaultLoginCallBack(userInfo)
+    // }, 3000)
   }, [])
 
   useEffect(() => {
     lottie.loadAnimation({
-      container: container.current,
-      renderer: "svg",
+      container: containerIMg.current,
+      renderer: 'svg',
       loop: false,
       autoplay: false,
-      animationData: require("images/loginSafe.json")
-    });
+      animationData: require('images/loginSafe.json')
+    })
 
     lottie.play()
     return () => {
-      lottie.destroy();
-    };
-  }, []);
-
+      lottie.destroy()
+    }
+  }, [])
 
   return (
     <div className={styles.enabled}>
-       <div  ref={container} className={styles.imgContainer}></div>
-      <h2>
-        {newGetStarted.enableTwoFactor} <br />
-        {newGetStarted.authenticationEnabled} ✅
-      </h2>
-      <p className={styles.notice}>{newGetStarted.factorTip}</p>
+      <div ref={containerIMg} className={styles.imgContainer}></div>
+      <h3>{newGetStarted.authenticationSuccessful}</h3>
+      {/* <p className={styles.notice}>{newGetStarted.factorTip}</p> */}
     </div>
   )
 }
