ff724f3b18d844a955aa6e3aaa2c037ca05057a6
bossjob-matt <bossjob_matt@proton.me>
2023-07-17 18:47:06 +0800

    feat: update footer

--- components/Footer/components/Mobile/SocialLink.tsx
+++ components/Footer/components/Mobile/SocialLink.tsx
@@ -5,11 +5,11 @@ import SocialList from '../common/SocialList'
 
 const SocialLink = (props: any) => {
   const { data } = props
-  const { followUs, workingDate } = data?.foot || {}
+  const { followUs, workingDate, workTitle } = data?.foot || {}
 
   return (
     <div className={styles.footerMobileSocialLinks}>
-      <div className={styles.footerMobileSocialDate}>{workingDate}</div>
+      <div className={styles.footerMobileSocialDate}>{`${workTitle}: ${workingDate}`}</div>
       <div className={styles.footerMobileSocialList}>
         <div className={styles.footerMobileSocialLinksText}>{followUs}</div>
         <SocialList />
--- components/Footer/components/PC/SocialLink.tsx
+++ components/Footer/components/PC/SocialLink.tsx
@@ -7,11 +7,11 @@ import SocialList from '../common/SocialList'
 const SocialLink = (props: any) => {
   const { data } = props
 
-  const { followUs, workingDate } = data?.foot || {}
+  const { followUs, workingDate, workTitle } = data?.foot || {}
 
   return (
     <div className={styles.footerDesktopSocialLinks}>
-      <div className={styles.footerDesktopSocialDate}>{workingDate}</div>
+      <div className={styles.footerDesktopSocialDate}>{`${workTitle}: ${workingDate}`}</div>
       <div className={styles.footerDesktopSocialList}>
         <div className={styles.footerDesktopSocialLinkFollow}>{followUs}</div>
         <SocialList />
