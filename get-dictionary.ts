b67b918216fa769d6543e78de5afc14621aa2c5f
cctipmao <cctip_mao@proton.me>
2023-07-17 19:04:23 +0800

    fix: add jp lang

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -10,11 +10,15 @@ import zhErrorcode from './errorcode/zh-CN/errorcode.json'
 import idPageLanguage from './dictionaries/id-ID.json'
 import idErrorcode from './errorcode/id-ID/errorcode.json'
 
+import jpPageLanguage from './dictionaries/ja-JP.json'
+import jpErrorcode from './errorcode/ja-JP/errorcode.json'
+
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
 const enLanguage = { errorcode: enErrorcode, ...enPageLanguage }
 const zhLanguage = { errorcode: zhErrorcode, ...zhPageLanguage }
 const idLanguage = { errorcode: idErrorcode, ...idPageLanguage }
+const jpLanguage = { errorcode: jpErrorcode, ...jpPageLanguage }
 const dictionaries = {
   'en-US': () => enLanguage,
   'zh-CN': () => zhLanguage,
