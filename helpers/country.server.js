40bd52dfa818c0bbc776f5cf327ae2f8a031fc91
cctipmao <cctip_mao@proton.me>
2023-07-17 14:28:15 +0800

    fix: country and language

--- helpers/country.server.js
+++ helpers/country.server.js
@@ -1,9 +1,11 @@
 import { cookies } from 'next/headers'
+import { defaultLanguage, languages } from './country'
 
 export const getServerLang = () => {
   let path =
     typeof window === 'undefined' ? process.env.NEXT_PUBLIC_HOST_PATH : window.location.href
   path = path?.split?.('//')[1]?.split?.('/')?.[1] // https://dev.bossjob.sg/en-US/...
   const geoConfiguration = cookies().get('geoConfiguration')?.value || ''
-  return path || geoConfiguration.split('_')?.[1] || 'en-US'
+  const langValues = languages.map(item => item.value)
+  return langValues.includes(path) ? path : geoConfiguration.split('_')?.[1] || defaultLanguage()
 }
