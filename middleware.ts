f62d2aeeb1512760fbda6ac4e4a3796c60b4c6e9
cctipmao <cctip_mao@proton.me>
2023-07-18 15:30:06 +0800

    fix: sitemaps

--- middleware.ts
+++ middleware.ts
@@ -64,7 +64,8 @@ export function middleware(request: NextRequest) {
       'chat-redirect',
       'huawei-jobs',
       'job-sitemap',
-      'jobs.xml'
+      'jobs.xml',
+      'sitemap.xml'
       // Your other files in `public`
     ].filter(item => pathname.includes(item)).length > 0
   ) { return }
