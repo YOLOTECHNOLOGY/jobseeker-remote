1fb335754a2b0d7efa1d178ff8c3f355097c560d
lvchangyu <bossjob_mayday@proto.me>
2023-07-18 16:56:09 +0800

    fix:countryId

--- helpers/country.js
+++ helpers/country.js
@@ -46,7 +46,7 @@ const countryCounfig = [
       value: "Tokyo To",
     },
     currency: 'jpy',
-    id: 81
+    id: 105
   },
   {
     name: 'Indonesia',
@@ -61,7 +61,7 @@ const countryCounfig = [
       value: "Jakarta Pusat",
     },
     currency: 'idr',
-    id: 62
+    id: 96
   },
 ]
 
