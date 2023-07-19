e95145e0813f9b4a7f7c647919a676a01cacf3b0
bossjob-matt <bossjob_matt@proton.me>
2023-07-19 17:49:26 +0800

    fix: update get-started footer links

--- app/[lang]/get-started/page.tsx
+++ app/[lang]/get-started/page.tsx
@@ -5,13 +5,16 @@ import { buildComponentScript } from 'app/models/abstractModels/util'
 import { serverDataScript } from 'app/models/abstractModels/FetchServierComponents'
 import styles from './index.module.scss'
 import Main from './components/main'
+import Link from 'next/link'
 import { getDictionary } from 'get-dictionary'
+import classNames from 'classnames'
 
 const configs = getConfigs([['location_lists']])
 
 const Page = async (props: any) => {
   const { lang } = props.params
   const dictionary: any = await getDictionary(lang)
+  const { newGetStarted } = dictionary
 
   return (
     <>
@@ -20,6 +23,19 @@ const Page = async (props: any) => {
         <div className={styles.container}>
           <Main dictionary={dictionary} />
         </div>
+        <p className={classNames([styles.tips, styles.hide])}>
+          {newGetStarted.tips}
+          <Link
+            href={
+              process.env.ENV === 'development'
+                ? 'https://dev.employer.bossjob.com'
+                : 'https://employer.bossjob.com'
+            }
+            className={styles.AuthCTALink}
+          >
+            {newGetStarted.employer}
+          </Link>
+        </p>
       </div>
     </>
   )
