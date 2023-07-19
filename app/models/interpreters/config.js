d78149bf6fd0b0711189d9140a33245d3ac2ba10
cctipmao <cctip_mao@proton.me>
2023-07-19 10:54:50 +0800

    fix: config

--- app/models/interpreters/config.js
+++ app/models/interpreters/config.js
@@ -5,8 +5,7 @@ import { fetchConfigService } from "store/services/config/fetchConfig";
 import { registInterpreter, Result } from 'app/models/abstractModels/util';
 import { cache } from 'react'
 import { mergeDeepLeft } from 'ramda'
-import { cookies } from 'next/headers';
-import { configKey } from 'helpers/cookies'
+import { getLang } from 'helpers/country';
 
 
 const cachedConfig = cache(fetchConfigService)
@@ -26,8 +25,8 @@ export default usedConfigProps => {
     const interpreter = registInterpreter(command =>
         command.cata({
             fetchData: () => M((content) => {
-                const lang = cookies().get(configKey)?.value?.split('_')?.[1];
-                return cachedConfig(content?.params?.lang ?? lang).then(data => {
+                const lang = getLang();
+                return cachedConfig(lang).then(data => {
                     return Result.success({
                         config: usedConfigProps
                             .map(valueForKeyPath(data))
