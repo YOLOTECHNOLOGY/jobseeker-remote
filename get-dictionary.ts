a8bb7427ec8d8df6edca9f67980dd5f6fd36c3a7
cctipmao <cctip_mao@proton.me>
2023-07-21 16:23:53 +0800

    fix: error building

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -1,4 +1,5 @@
 // import 'server-only'
+import { getLang } from 'helpers/country';
 import type { Locale } from './i18n-config'
 import otaClient from '@crowdin/ota-client';
 // import enPageLanguage from './dictionaries/en-US.json'
@@ -30,8 +31,10 @@ const client = new otaClient('7ebf57665448382c18ccd49ef5z', {
   disableStringsCache: false,
   disableLanguagesCache: false
 })
-
+client.setCurrentLocale(getLang())
 export const getDictionary = async (locale: Locale) => {
+
+
   return client.getStringsByLocale(locale)
   // return dictionaries[locale]?.() || dictionaries['en-US']()
 }
