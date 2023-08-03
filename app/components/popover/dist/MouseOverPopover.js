cca4220e31dccbda0817d596bb2b6db361a9d2fb
mayday <bossjob_mayday@proto.me>
2023-08-03 18:27:37 +0800

    fix: code style

--- app/components/popover/dist/MouseOverPopover.js
+++ app/components/popover/dist/MouseOverPopover.js
@@ -1,33 +1,35 @@
 "use strict";
 exports.__esModule = true;
 exports.MouseOverPopover = exports.isContentOverflowing = void 0;
-var react_1 = require("react");
-var index_module_scss_1 = require("../../[lang]/company/[keyword]/components/InfoList/index.module.scss");
-var Popover_1 = require("@mui/material/Popover");
-var Typography_1 = require("@mui/material/Typography");
-var link_1 = require("next/link");
-var classnames_1 = require("classnames");
-var InfoList_1 = require("../../[lang]/company/[keyword]/components/InfoList");
+const react_1 = require("react");
+const index_module_scss_1 = require("../../[lang]/company/[keyword]/components/InfoList/index.module.scss");
+const Popover_1 = require("@mui/material/Popover");
+const Typography_1 = require("@mui/material/Typography");
+const link_1 = require("next/link");
+const classnames_1 = require("classnames");
+const InfoList_1 = require("../../[lang]/company/[keyword]/components/InfoList");
 function isContentOverflowing(element) {
     console.log(' element?.scrollWidth ', element, element === null || element === void 0 ? void 0 : element.scrollWidth, element === null || element === void 0 ? void 0 : element.clientWidth);
     return (element === null || element === void 0 ? void 0 : element.scrollWidth) > (element === null || element === void 0 ? void 0 : element.clientWidth);
 }
 exports.isContentOverflowing = isContentOverflowing;
 function MouseOverPopover(props) {
-    var _a;
-    var ref = react_1.useRef(null);
-    var _b = react_1.useState(false), showPop = _b[0], setShow = _b[1];
-    var _c = react_1["default"].useState(null), anchorEl = _c[0], setAnchorEl = _c[1];
-    var is_url = InfoList_1.isURL(props.value);
-    var handlePopoverOpen = function (event) {
+    const _a;
+    const ref = react_1.useRef(null);
+    const _b = react_1.useState(false); const showPop = _b[0];
+     const setShow = _b[1];
+    const _c = react_1["default"].useState(null);
+     const anchorEl = _c[0]; const setAnchorEl = _c[1];
+    const is_url = InfoList_1.isURL(props.value);
+    const handlePopoverOpen = function (event) {
         if (!showPop)
             return;
         setAnchorEl(event.currentTarget);
     };
-    var handlePopoverClose = function () {
+    const handlePopoverClose = function () {
         setAnchorEl(null);
     };
-    var open = Boolean(anchorEl);
+    const open = Boolean(anchorEl);
     react_1.useLayoutEffect(function () {
         if (isContentOverflowing(ref.current)) {
             setShow(true);
