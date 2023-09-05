253bb45faba2d4c9d0ebe0c79f2bfe74539f88da
sunnibfgi <ja1523@proton.me>
2023-09-05 15:41:04 +0800

    fix: vip avatar

--- app/[lang]/get-started/components/userAvatar.tsx
+++ app/[lang]/get-started/components/userAvatar.tsx
@@ -1,10 +1,15 @@
 'use client'
 import { useSelector } from 'react-redux'
+import { useSearchParams } from 'next/navigation'
+
 import styles from '../index.module.scss'
 
 export default function UserAvatar({ avatar }) {
   const userDetail = useSelector((store: any) => store.users.fetchUserOwnDetail?.response ?? {})
-  if (userDetail?.vip?.is_vip) {
+  const searchParams = useSearchParams()
+
+  console.log('userDetail:', userDetail)
+  if (userDetail?.vip?.is_vip || searchParams.get('is_vip')) {
     return (
       <div className={styles.vipAvatar}>
         <img src={require('./vip_user_icon.png').default.src} width="51" height="20" alt='avatar' style={{
