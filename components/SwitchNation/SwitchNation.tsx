67207c2afd9bb3613aca498b4934ab34a5c919a8
sunnibfgi <ja1523@proton.me>
2023-09-08 09:55:33 +0800

    fix: searchparams

--- components/SwitchNation/SwitchNation.tsx
+++ components/SwitchNation/SwitchNation.tsx
@@ -154,7 +154,8 @@ const SwitchNation = ({ close, open, lang }: propsType) => {
 
     } else {
 
-      query += '/' + pathname.split('/').slice(2).join('/')
+      query += '/' + pathname.split('/').slice(2).join('/') + '?' + referralCodeParams + invitedSourceParams
+
 
     }
 
