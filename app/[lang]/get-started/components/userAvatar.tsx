e4f9f6e9f9a92dfd0fc35fd1661366525196e2ca
sunnibfgi <ja1523@proton.me>
2023-09-11 10:52:57 +0800

    fix: search type

--- app/[lang]/get-started/components/userAvatar.tsx
+++ app/[lang]/get-started/components/userAvatar.tsx
@@ -8,8 +8,7 @@ export default function UserAvatar({ avatar }) {
   const userDetail = useSelector((store: any) => store.users.fetchUserOwnDetail?.response ?? {})
   const searchParams = useSearchParams()
 
-  console.log('userDetail:', userDetail)
-  if (userDetail?.vip?.is_vip || searchParams.get('is_vip')) {
+  if (userDetail?.vip?.is_vip || Number(searchParams.get('is_vip'))) {
     return (
       <div className={styles.vipAvatar}>
         <img src={require('./vip_user_icon.png').default.src} width="51" height="20" alt='avatar' style={{
